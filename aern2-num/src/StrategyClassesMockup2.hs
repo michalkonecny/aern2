55646544f0f085f73b84c1b1c77125653f8b53aa
Michal Konečný <mikkonecny@gmail.com>
2016-01-19 15:50:25 +0000

    Improved draft of strategy-generic expressions.

--- aern2-num/src/StrategyClassesMockup2.hs
+++ aern2-num/src/StrategyClassesMockup2.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE TypeFamilies, TypeSynonymInstances, FlexibleInstances, FunctionalDependencies, RankNTypes #-}
+{-# LANGUAGE TypeFamilies, FlexibleInstances, RankNTypes #-}
 --{-# LANGUAGE PolyKinds #-}
 
 module StrategyClassesMockup2 where
@@ -9,6 +9,32 @@ module StrategyClassesMockup2 where
 import AERN2.Num
 
 import Control.Arrow
+
+
+exampleRealGeneric1 :: RealGeneric (Pair R R) R
+exampleRealGeneric1 = 
+    RealGeneric (anyStrategy addA)
+
+exampleRealGeneric1EvalWithOrdFuncReal :: (CauchyReal, CauchyReal) -> CauchyReal
+exampleRealGeneric1EvalWithOrdFuncReal =
+    withEvalStrategyReal exampleRealGeneric1 EvalWithOrdFuncReal
+
+exampleRealGeneric2 :: RealGeneric R (Pair R R)
+exampleRealGeneric2 = 
+    RealGeneric (anyStrategy $ proc x -> do nx <- negA -< x; returnA -< (x,nx))
+
+exampleRealGeneric2EvalWithOrdFuncReal :: CauchyReal -> (CauchyReal, CauchyReal)
+exampleRealGeneric2EvalWithOrdFuncReal =
+    withEvalStrategyReal exampleRealGeneric2 EvalWithOrdFuncReal
+
+exampleRealGeneric3 :: RealGeneric R (List R)
+exampleRealGeneric3 = 
+    RealGeneric (anyStrategy $ proc x -> do nx <- negA -< x; returnA -< [x,nx])
+
+exampleRealGeneric3EvalWithOrdFuncReal :: CauchyReal -> [CauchyReal]
+exampleRealGeneric3EvalWithOrdFuncReal =
+    withEvalStrategyReal exampleRealGeneric3 EvalWithOrdFuncReal
+
 
 {-| strategy for evaluating arrow-generic real expressions  -}
 class
@@ -29,55 +55,42 @@ instance EvalStrategyReal EvalWithOrdFuncReal where
     type ES_b EvalWithOrdFuncReal = Accuracy -> Maybe Bool
 
 {-| Strategy-generic expression -}
-data RealGenericFixed = RealGenericFixed
-    { 
-        withEvalStrategyRealFixed :: 
-            (forall s. EvalStrategyReal s => s -> (ES_to s) (ES_r s) (ES_r s)) 
-    }
-
-exampleRealGenericFixed1 :: RealGenericFixed
-exampleRealGenericFixed1 =
-    RealGenericFixed (const negA)
-
 data RealGeneric i o = RealGeneric
     { 
         withEvalStrategyReal :: 
-            (forall s. EvalStrategyReal s => s -> (ES_to s) (Unwrapped (i s)) (o s)) 
+            (forall s. EvalStrategyReal s => s -> (ES_to s) (Unwrapped (i s)) (Unwrapped (o s))) 
     }
 
 class Wrapper w where
     type Unwrapped w
-    wrap :: w -> (Unwrapped w) -> w
+    wrap :: w {-^ dummy parameter, only for type inference -} -> (Unwrapped w) -> w
     unwrap :: w -> (Unwrapped w)
     
 
-{-| This trivial synonym helps to make expressions more readable, eg: 
-    @RealGeneric (anyStrategy $ arr R . addA)@
+{-| This trivial synonym helps to make expressions that return one real number more readable, eg: 
+    @RealGeneric (anyStrategy addA)@
 -}
-getR ::
-          Arrow to =>
-          a `to` (ES_r s) -> b -> a `to` (R s)
-getR fnA = const $ arr R . fnA
+anyStrategy :: a -> s -> a
+anyStrategy = const
 
 data R s = R { unR :: ES_r s }
-data RR s = RR { unRR :: (ES_r s, ES_r s) }
-data RL s = RL { unRL :: [ES_r s] }
+
+data Pair t1 t2 s = Pair { unPair :: (t1 s, t2 s) }
+data List t s = List { unList :: [t s] }
 
 instance Wrapper (R s) where
     type Unwrapped (R s) = ES_r s
     wrap _ = R
     unwrap = unR
 
-instance Wrapper (RR s) where
-    type Unwrapped (RR s) = (ES_r s, ES_r s)
-    wrap _ = RR
-    unwrap = unRR
+instance (Wrapper (t1 s), Wrapper (t2 s)) => Wrapper (Pair t1 t2 s) where
+    type Unwrapped (Pair t1 t2 s) = (Unwrapped (t1 s), Unwrapped (t2 s))
+    wrap (Pair (sample1, sample2)) (a,b) = Pair (wrap sample1 a, wrap sample2 b)
+    unwrap (Pair (a,b)) = (unwrap a, unwrap b)
 
-exampleRealGeneric1 :: RealGeneric RR R
-exampleRealGeneric1 = RealGeneric (getR addA)
-
-
-exampleRealGeneric1EvalWithOrdFuncReal :: (CauchyReal, CauchyReal) -> CauchyReal
-exampleRealGeneric1EvalWithOrdFuncReal =
-    unwrap . withEvalStrategyReal exampleRealGeneric1 EvalWithOrdFuncReal
+instance (Wrapper (t s)) => Wrapper (List t s) where
+    type Unwrapped (List t s) = [Unwrapped (t s)]
+    wrap (List (sample:_)) items = List $ map (wrap sample) items
+    wrap (List []) _ = error "instance Wrapper (List t s): wrap called with an empty sample list" 
+    unwrap (List items) = map unwrap items
 
