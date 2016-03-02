966238d1ddcf77a8ffa587253cfaef4f29090ad2
Michal Konečný <mikkonecny@gmail.com>
2016-03-02 23:34:57 +0000

    Add a comment outlining a method of bounding the error of DCT polynomial division using polynomial maximisation.

--- aern2-fnreps/src/FnReps/Polynomial/UnaryCheb/Poly/Division.hs
+++ aern2-fnreps/src/FnReps/Polynomial/UnaryCheb/Poly/Division.hs
@@ -8,7 +8,11 @@ divideDCT_terms :: Degree -> Terms -> Terms -> Terms
 divideDCT_terms d = lift2_DCT (const $ const $ d) (/)
     {- 
         TODO
-        Add a bound on the interpolation error.
+        Add a bound on the interpolation error.  Use the following formula:
+        
+        error <= (max(p(x)- r(x)*q(x))) / (min|q(x)|)
+        
+        where r(x) is our approximation of p(x)/q(x). 
     -}
 
 
