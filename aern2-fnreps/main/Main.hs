0e3a866048d6194c61608ce764e02f297c9e9f29
Michal Konečný <mikkonecny@gmail.com>
2016-03-09 11:07:20 +0000

    Improved the output of the aern2-fnreps executable.

--- aern2-fnreps/main/Main.hs
+++ aern2-fnreps/main/Main.hs
@@ -8,11 +8,19 @@ import FnReps.Polynomial.UnaryCheb.PolyBall
 main :: IO ()
 main =
     do
-    putStrLn $ show $ divT10
-    putStrLn $ show $ getApproximate (bits 30) $ divT10
+    printWithName "1/(10x^2+1) degree 10" divT10d10
+    printWithName "1/(10x^2+1) degree 10, APPROX:" $ getApproximate (bits 30) divT10d10
+    printWithName "1/(10x^2+1) degree 20" divT10d20
+    printWithName "1/(10x^2+1) degree 20, APPROX:" $ getApproximate (bits 30) divT10d20
 --    putStrLn $ show $ getApproximate (bits 100) $ 1 / (10 * x^2 + 1)
     where
-    divT10 = divT 20 10
+    divT10d10 = divT 10 10
+    divT10d20 = divT 20 10
     divT d n = 1 / (setMaxDegree d ((n * x*x) + 1))
     x = projUnaryFnA (Interval (-1.0) 1.0) :: PolyBall
+    
+    printWithName name value =
+        putStrLn $ banner ++ "\n" ++ name ++ ":\n" ++ show value
+        where
+        banner = replicate (int 100) '='
 
