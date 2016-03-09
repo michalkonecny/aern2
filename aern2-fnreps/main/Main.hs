d46b9f20ae1e0e7fff4329cda4c701bceb0029ca
Michal Konečný <mikkonecny@gmail.com>
2016-03-09 11:44:29 +0000

    Further improved the output of the aern2-fnreps executable.  Added enclosures for 1/(100x^2+1).

--- aern2-fnreps/main/Main.hs
+++ aern2-fnreps/main/Main.hs
@@ -8,19 +8,26 @@ import FnReps.Polynomial.UnaryCheb.PolyBall
 main :: IO ()
 main =
     do
-    printWithName "1/(10x^2+1) degree 10" divT10d10
-    printWithName "1/(10x^2+1) degree 10, APPROX:" $ getApproximate (bits 30) divT10d10
-    printWithName "1/(10x^2+1) degree 20" divT10d20
-    printWithName "1/(10x^2+1) degree 20, APPROX:" $ getApproximate (bits 30) divT10d20
---    putStrLn $ show $ getApproximate (bits 100) $ 1 / (10 * x^2 + 1)
+    {- A plot of the following enclosures of 1/(10x^2+1) 
+        is at http://fooplot.com/plot/2yt7q2sevi
+    -}
+    printWithName "1/(10x^2+1) degree 10" $ divT10 10
+    printWithName "1/(10x^2+1) degree 14" $ divT10 14
+    printWithName "1/(10x^2+1) degree 18" $ divT10 18
+    
+    printWithName "1/(100x^2+1) degree 30" $ divT100 30
+    printWithName "1/(100x^2+1) degree 40" $ divT100 40
+    printWithName "1/(100x^2+1) degree 50" $ divT100 50
     where
-    divT10d10 = divT 10 10
-    divT10d20 = divT 20 10
+    divT10 d = divT d 10
+    divT100 d = divT d 100
     divT d n = 1 / (setMaxDegree d ((n * x*x) + 1))
     x = projUnaryFnA (Interval (-1.0) 1.0) :: PolyBall
     
     printWithName name value =
+        do
         putStrLn $ banner ++ "\n" ++ name ++ ":\n" ++ show value
+        putStrLn $ "\nAPPROX:\n" ++ show (getApproximate (bits 30) value)
         where
         banner = replicate (int 100) '='
 
