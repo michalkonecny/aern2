a9929094855a7d43129efff0b001eedfa1cc97d0
Michal Konečný <mikkonecny@gmail.com>
2016-03-16 23:44:36 +0000

    Computing the range of MPBall -> MPBall functions.

--- aern2-num/src/AERN2/Net/Strategy/Direct.hs
+++ aern2-num/src/AERN2/Net/Strategy/Direct.hs
@@ -1,13 +1,17 @@
-{-# LANGUAGE CPP, FlexibleInstances #-}
+{-# LANGUAGE CPP, FlexibleInstances, FlexibleContexts #-}
 module AERN2.Net.Strategy.Direct 
 (
+    ufnB2B_x, ufnB2B_10x2p1, ufnB2B_1o10x2p1, ufnB2B_sinx,
     Interval(..), rati2MPBall, UnaryFnMPBall(..), UnaryFnCR(..)
 )
 where
 
 import AERN2.Num
+import qualified Prelude as P
 
 import AERN2.RealFunction
+
+import qualified Data.PQueue.Max as Q -- used in a range algorithm
 
 --import qualified Data.Map as Map
 
@@ -22,75 +26,27 @@ maybeTrace
     | shouldTrace = trace
     | otherwise = const id
 
+{- mini examples -}
 
-rati2MPBall :: Interval Rational -> MPBall
-rati2MPBall il@(Interval l r) =
-    maybeTrace
-    (
-        "rati2MPBall: " ++ show il
-    ) $
-    endpoints2Ball lMP rMP
-    where
-    lMP = q2MP l
-    rMP = q2MP r
-    q2MP q =
-        case nl of
-            NormBits i -> rational2BallP (prec (max 10 (10 - i))) q
-            NormZero -> error "rati2MPBall does not work for a singleton interval"
-    nl = getNormLog (r - l)
+ufnB2B_x :: UnaryFnMPBall
+ufnB2B_x = projUnaryFnA (Interval (-1.0) 2.0)
 
-_mpBall2rati :: MPBall -> Interval Rational
-_mpBall2rati b =
-    Interval l r
-    where
-    l = toRationalDown b
-    r = toRationalUp b
+ufnB2B_1o10x2p1 :: UnaryFnMPBall
+ufnB2B_1o10x2p1= 
+    UnaryFnMPBall (Interval (-1.0) 2.0) $
+        \ b -> 1/((10*b*b)+1)
 
-_cri2MPBall :: Interval CauchyReal -> MPBall
-_cri2MPBall (Interval l r) =
-    maybeTrace
-    (
-        "cri2MPBall: Interval " ++ show lMP ++ " " ++ show rMP
-    ) $
-    endpoints2Ball lMP rMP
-    where
-    lMP = cauchyReal2ball l a
-    rMP = cauchyReal2ball r a
-    a =
-        case nl of
-            NormBits i -> bits (max 10 (1000 - i))
-            NormZero -> error "cri2MPBall does not work for a singleton interval"
-    nl = getNormLog ((cauchyReal2ball r a0) - (cauchyReal2ball l a0))
-    a0 = bits 10
+ufnB2B_10x2p1 :: UnaryFnMPBall
+ufnB2B_10x2p1= 
+    UnaryFnMPBall (Interval (-1.0) 2.0) $
+        \ b -> ((10*b*b)+1)
 
-mpBall2cri :: MPBall -> Interval CauchyReal
-mpBall2cri b =
-    maybeTrace
-    (
-        "mpBall2cri: b = " ++ show b
-    ) $
-    Interval l r
-    where
-    l = convergent2CauchyReal Nothing $ repeat lMP
-    r = convergent2CauchyReal Nothing $ repeat rMP
-    (lMP, rMP) = ball2endpoints b
+ufnB2B_sinx :: UnaryFnMPBall
+ufnB2B_sinx= 
+    UnaryFnMPBall (Interval (-1.0) 10.0) $
+        \ b -> (sin b)
 
-{-
--- the following instance is currently not used
-instance RationalIntervalA (->) (Interval CauchyReal) where
-    type (IntervalE (Interval CauchyReal)) = CauchyReal
-    type (IntervalR (Interval CauchyReal)) = CauchyReal
-    getEndpointsA (Interval l r) = (l,r)
-    fromEndpointsA (l,r) = Interval l r
-    limitIntervalsToRealA sq = convergent2CauchyReal Nothing $ map cri2MPBall sq
-
-instance RationalIntervalA (->) (Interval Rational) where
-    type (IntervalE (Interval Rational)) = Rational
-    type (IntervalR (Interval Rational)) = CauchyReal
-    getEndpointsA (Interval l r) = (l, r)
-    fromEndpointsA (l,r) = (Interval l r)
-    limitIntervalsToRealA sq = convergent2CauchyReal Nothing $ map rati2MPBall sq
--}
+-- rangeOnIntervalUnaryFnA (ufnB2B_sinx, Interval (-1.0) 10.0)
 
 {- TODO The following function types should move to aern2-function, when it is created -}
 
@@ -99,7 +55,7 @@ data UnaryFnMPBall =
     {
         ufnB2B_dom :: Interval Rational, 
         ufnB2B_eval :: MPBall -> MPBall
-    } 
+    }
 
 instance RealUnaryFnA (->) UnaryFnMPBall where
     type UnaryFnDomPoint UnaryFnMPBall = Rational
@@ -114,9 +70,72 @@ instance RealUnaryFnA (->) UnaryFnMPBall where
     evalAtDomPointUnaryFnA (UnaryFnMPBall _dom f, r) = 
         convergent2CauchyReal Nothing $ 
             map f $ map (flip rational2BallP r) standardPrecisions
-    rangeOnIntervalUnaryFnA (UnaryFnMPBall _dom f, ri) =  
-        mpBall2cri $ f (rati2MPBall ri) -- TODO: not computing the maximum extension, should do 
+    rangeOnIntervalUnaryFnA (UnaryFnMPBall _dom f, ri) =
+        Interval l r
+        where
+        l = convergent2CauchyReal Nothing minSequence
+        r = convergent2CauchyReal Nothing maxSequence
+        maxSequence = search fi friL $ Q.singleton $ MaxSearchSegment ri friL friR
+            where
+            (friL, friR) = ball2endpoints fri
+            fri = fi ri
+            fi = f . rati2MPBall
+        minSequence = map negate $ search fi friL $ Q.singleton $ MaxSearchSegment ri friL friR
+            where
+            (friL, friR) = ball2endpoints fri
+            fri = fi ri
+            fi = negate . f . rati2MPBall
+        search fi prevL prevQueue =
+            maybeTrace 
+            (
+                "rangeOnIntervalUnaryFnA: search:" 
+                ++ "\n  seg = " ++ show seg
+                ++ "\n  nextL = " ++ show nextL
+                ++ "\n  segValR = " ++ show segValR
+            ) $
+            (endpoints2Ball nextL segValR) : 
+                search fi nextL nextQueue12
+            where
+            (MaxSearchSegment seg segValL segValR, rest) = Q.deleteFindMax prevQueue
+            nextL = segValL `max` prevL
+            (seg1, seg2) = splitInterval seg
+            (seg1ValL, seg1ValR) = ball2endpoints $ fi seg1
+            (seg2ValL, seg2ValR) = ball2endpoints $ fi seg2
+            seg1NoMax = (seg1ValR <= nextL) == Just True 
+            seg2NoMax = (seg2ValR <= nextL) == Just True
+            nextQueue1 =
+                if seg1NoMax then rest else Q.insert seg1E rest
+            nextQueue12 =
+                if seg2NoMax then nextQueue1 else Q.insert seg2E nextQueue1
+            seg1E = MaxSearchSegment seg1 seg1ValL seg1ValR
+            seg2E = MaxSearchSegment seg2 seg2ValL seg2ValR
 
+data MaxSearchSegment =
+    MaxSearchSegment 
+    {
+        _maxSearchSegment_seg :: Interval Rational,
+        _maxSearchSegment_lowerBnd :: MPBall, -- should be exact
+        _maxSearchSegment_upperBnd :: MPBall -- should be exact
+    }
+
+instance Eq MaxSearchSegment where
+    (MaxSearchSegment (Interval l1 r1) _ _) == (MaxSearchSegment (Interval l2 r2) _ _) =
+        l1 == l2 && r1 == r2
+instance Ord MaxSearchSegment where
+    compare (MaxSearchSegment _ _ u1) (MaxSearchSegment _ _ u2) =
+        case (u1 < u2, u1 > u2) of
+            (Just True, _) -> P.LT
+            (_, Just True) -> P.GT
+            _ -> P.EQ
+
+splitInterval ::
+    (CanDivBy a Integer, CanAddSameTypeA (->) a) => 
+    Interval a -> (Interval a, Interval a)
+splitInterval (Interval l r) =
+    (Interval l m, Interval m r)
+    where
+    m = (l + r) / 2 
+    
 data UnaryFnCR =
     UnaryFnCR
     {
@@ -137,4 +156,58 @@ instance RealUnaryFnA (->) UnaryFnCR
     rangeOnIntervalUnaryFnA (UnaryFnCR _dom _f, _ri) = 
         error "rangeOnIntervalUnaryFnA not implemented for UnaryFnCR"
 
+
+{- utilities -}
+
+rati2MPBall :: Interval Rational -> MPBall
+rati2MPBall _il@(Interval l r) =
+--    maybeTrace
+--    (
+--        "rati2MPBall: " ++ show _il
+--    ) $
+    endpoints2Ball lMP rMP
+    where
+    lMP = q2MP l
+    rMP = q2MP r
+    q2MP q =
+        case nl of
+            NormBits i -> rational2BallP (prec (max 10 (10 - i))) q
+            NormZero -> error "rati2MPBall does not work for a singleton interval"
+    nl = getNormLog (r - l)
+
+_mpBall2rati :: MPBall -> Interval Rational
+_mpBall2rati b =
+    Interval l r
+    where
+    l = toRationalDown b
+    r = toRationalUp b
+
+_cri2MPBall :: Interval CauchyReal -> MPBall
+_cri2MPBall (Interval l r) =
+--    maybeTrace
+--    (
+--        "cri2MPBall: Interval " ++ show lMP ++ " " ++ show rMP
+--    ) $
+    endpoints2Ball lMP rMP
+    where
+    lMP = cauchyReal2ball l a
+    rMP = cauchyReal2ball r a
+    a =
+        case nl of
+            NormBits i -> bits (max 10 (1000 - i))
+            NormZero -> error "cri2MPBall does not work for a singleton interval"
+    nl = getNormLog ((cauchyReal2ball r a0) - (cauchyReal2ball l a0))
+    a0 = bits 10
+
+_mpBall2cri :: MPBall -> Interval CauchyReal
+_mpBall2cri b =
+--    maybeTrace
+--    (
+--        "mpBall2cri: b = " ++ show b
+--    ) $
+    Interval l r
+    where
+    l = convergent2CauchyReal Nothing $ repeat lMP
+    r = convergent2CauchyReal Nothing $ repeat rMP
+    (lMP, rMP) = ball2endpoints b
 
