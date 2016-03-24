5f1e2efd14515edaf0c6a25111c19a3c22af6610
Michal Konečný <mikkonecny@gmail.com>
2016-03-24 22:11:11 +0000

    Fresh start on the executable.  Now supporting multiple operations, functions, function representations and effort settings.  Currently only one function and small number of operation/representations supported.

--- aern2-fnreps/main/Main.hs
+++ aern2-fnreps/main/Main.hs
@@ -1,12 +1,94 @@
 module Main where
 
 import AERN2.Num
+
+import qualified Data.Map as Map
+
+import System.Environment
+
 import AERN2.RealFunction
+import AERN2.Net
 import FnReps.Polynomial.UnaryCheb.PolyBall
 --import FnReps.Polynomial.UnaryCheb.RealFn
 
-main :: IO ()
 main =
+    do
+    args <- getArgs
+    (computationDescription, result) <- processArgs args 
+    putStrLn $ computationDescription
+    putStrLn $ "result = " ++ show result
+    putStrLn $ "result ~ " ++ showB result
+    putStrLn $ "accuracy: " ++ (show $ getAccuracy result)
+    where
+    showB = show . getApproximate (bits 30)
+--    showAP = show . getApproximate (bits 50) . cheb2Power
+
+processArgs :: 
+    [String] -> 
+    IO (String, MPBall)
+processArgs (operationCode : functionCode : representationCode : effortArgs) =
+    return (computationDescription, result)
+    where
+    computationDescription =
+        "computing " ++ operationCode ++ "  " ++ fnDescription
+    
+    result = 
+        case (representationCode, operationCode) of
+            ("fun", "max") -> maxFun fnB2B accuracy 
+            ("poly", "max") -> maxPB $ fnPB p maxDeg 
+            ("poly", "integrate") -> integratePB $ fnPB p maxDeg 
+    (Just (fnDescription, fnPB, fnB2B)) = Map.lookup functionCode functions
+    maxDeg = read maxDegS
+    p = prec $ read precS
+    [precS, maxDegS] = effortArgs
+    accuracy = bits $ read accuracyS
+    [accuracyS] = effortArgs
+
+    integratePB :: PolyBall -> MPBall
+    integratePB b =
+        integrateUnaryFnA (b, mpBall domL, mpBall domR) 
+        where
+        Interval domL domR = ball_domain b
+    maxPB :: PolyBall -> MPBall
+    maxPB b =
+        m
+        where
+        Interval _ m = rangeOnIntervalUnaryFnA (b, domain) 
+        domain = ball_domain b
+
+    maxFun :: UnaryFnMPBall -> Accuracy -> MPBall
+    maxFun fn ac =
+        cauchyReal2ball m ac
+        where
+        Interval _ m = rangeOnIntervalUnaryFnA (fn, domain) 
+        domain = ufnB2B_dom fn
+
+functions :: Map.Map String (String, Precision -> Degree -> PolyBall, UnaryFnMPBall)
+functions =
+    Map.fromList
+    [
+        ("sinesine", (analyticFn1_Name, analyticFn1_PB, analyticFn1_B2B))
+    ]
+
+analyticFn1_Name :: String
+analyticFn1_Name = "sin(10x+sin(20x^2)) over [-1,1]"
+
+analyticFn1_PB :: Precision -> Degree -> PolyBall
+analyticFn1_PB p d =
+    sin(10*x + sin(20*x*x))
+    where
+    x = 
+        setMaxDegree d $
+        setPrecision p $
+        projUnaryFnA (Interval (-1.0) 1.0)
+
+analyticFn1_B2B :: UnaryFnMPBall
+analyticFn1_B2B =
+    UnaryFnMPBall (Interval (-1.0) 1.0) $
+    \x -> sin(10*x + sin(20*x*x))
+
+main2 :: IO ()
+main2 =
     do
     {- A plot of the following enclosures of 1/(10x^2+1) is at: 
         http://fooplot.com/plot/2yt7q2sevi
