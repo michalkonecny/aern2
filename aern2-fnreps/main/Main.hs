70d547237f2113c6874261673bc2dcfc9e0b35c1
Michal Konečný <mikkonecny@gmail.com>
2016-05-07 23:35:38 +0100

    Start in integrating PPoly benchmarks to Main.

--- aern2-fnreps/main/Main.hs
+++ aern2-fnreps/main/Main.hs
@@ -10,6 +10,7 @@ import AERN2.RealFunction
 import AERN2.Net
 import FnReps.Polynomial.UnaryCheb.PolyBall
 import FnReps.Polynomial.UnaryCheb.Poly (compose,absX,absXshifted, reduceDegreeAndSweep)
+import FnReps.PiecewisePolynomial.UnaryCheb.PPoly.Benchmarks
 --import FnReps.Polynomial.UnaryCheb.RealFn
 
 main :: IO ()
@@ -41,14 +42,21 @@ processArgs (operationCode : functionCode : representationCode : effortArgs) =
             ("fun", "integrate") -> integrateFun fnB2B accuracy 
             ("poly", "max") -> maxPB $ fnPB p maxDeg 
             ("poly", "integrate") -> integratePB $ fnPB p maxDeg
+            ("ppoly", "max") -> fnPP OpMax pp_maxDeg pp_divThreshold pp_divIts pp_rangeAcc
+            ("ppoly", "integrate") -> fnPP OpIntegrate pp_maxDeg pp_divThreshold pp_divIts pp_rangeAcc
             _ -> error $ "unknown (representationCode, operationCode): " ++ show (representationCode, operationCode)
-    (Just (fnDescription, fnPB, fnB2B)) = Map.lookup functionCode functions
+    (Just (fnDescription, fnPB, fnB2B, fnPP)) = Map.lookup functionCode functions
     maxDeg = read maxDegS
     p = prec $ read precS
     rangeBits = bits $ read rangeBitsS
     [precS, maxDegS, rangeBitsS] = effortArgs
     accuracy = bits $ read accuracyS
     [accuracyS] = effortArgs
+    pp_maxDeg = read pp_maxDegS
+    pp_divThreshold = bits $ read pp_divThresholdS
+    pp_divIts = read pp_divItsS
+    pp_rangeAcc = bits $ read pp_rangeBitsS
+    [pp_maxDegS, pp_divThresholdS, pp_divItsS, pp_rangeBitsS] = effortArgs
 
     integratePB :: PolyBall -> MPBall
     integratePB b =
@@ -77,16 +85,16 @@ processArgs (operationCode : functionCode : representationCode : effortArgs) =
         r = integrateUnaryFnA (fn, cauchyReal domL, cauchyReal domR) 
         Interval domL domR = ufnB2B_dom fn
 
-functions :: Map.Map String (String, Precision -> Degree -> PolyBall, UnaryFnMPBall)
+functions :: Map.Map String (String, Precision -> Degree -> PolyBall, UnaryFnMPBall, FnPP)
 functions =
     Map.fromList
     [
-        ("sinesine", (analyticFn1_Name, analyticFn1_PB, analyticFn1_B2B)),
-        ("sine+cos", (analyticFn2_Name, analyticFn2_PB, analyticFn2_B2B)),
-        ("fraction", (nearsingulatityFn1_Name, nearsingulatityFn1_PB, nearsingulatityFn1_B2B)),
-        ("fraction-periodic", (nearsingulatityPerFn1_Name, nearsingulatityPerFn1_PB, nearsingulatityPerFn1_B2B)),
-        ("abs", (nonsmoothFn1_Name, nonsmoothFn1_PB, nonsmoothFn1_B2B)),
-        ("bumpy", (nonsmoothFn2_Name, nonsmoothFn2_PB, nonsmoothFn2_B2B))
+        ("sinesine", (analyticFn1_Name, analyticFn1_PB, analyticFn1_B2B, analyticFn1_PP)),
+        ("sine+cos", (analyticFn2_Name, analyticFn2_PB, analyticFn2_B2B, analyticFn2_PP)),
+        ("fraction", (nearsingulatityFn1_Name, nearsingulatityFn1_PB, nearsingulatityFn1_B2B, nearsingulatityFn1_PP)),
+        ("fraction-periodic", (nearsingulatityPerFn1_Name, nearsingulatityPerFn1_PB, nearsingulatityPerFn1_B2B, nearsingulatityPerFn1_PP)),
+        ("abs", (nonsmoothFn1_Name, nonsmoothFn1_PB, nonsmoothFn1_B2B, nonsmoothFn1_PP)),
+        ("bumpy", (nonsmoothFn2_Name, nonsmoothFn2_PB, nonsmoothFn2_B2B, nonsmoothFn2_PP))
     ]
 
 analyticFn1_Name :: String
@@ -106,6 +114,13 @@ analyticFn1_B2B =
     UnaryFnMPBall (Interval (-1.0) 1.0) $
     \x -> catchingExceptions $ sin(10*x + sin(20*x*x))
 
+data Operator = OpMax | OpIntegrate
+
+type FnPP = Operator -> Degree -> Accuracy -> Integer -> Accuracy -> MPBall
+
+analyticFn1_PP :: Operator -> Degree -> Accuracy -> Integer -> Accuracy -> MPBall
+analyticFn1_PP = error $ "Not supporting PPoly for: " ++ analyticFn1_Name
+
 analyticFn2_Name :: String
 analyticFn2_Name = "sin(10x)+cos(20x) over [-1,1]"
 
@@ -122,6 +137,9 @@ analyticFn2_B2B :: UnaryFnMPBall
 analyticFn2_B2B =
     UnaryFnMPBall (Interval (-1.0) 1.0) $
     \x -> catchingExceptions $ sin(10*x)+cos(20*x)
+
+analyticFn2_PP :: Operator -> Degree -> Accuracy -> Integer -> Accuracy -> MPBall
+analyticFn2_PP = error $ "Not supporting PPoly for: " ++ analyticFn2_Name
 
 nearsingulatityFn1_Name :: String
 nearsingulatityFn1_Name = "1/(100x^2+1) over [-1,1]"
@@ -140,6 +158,12 @@ nearsingulatityFn1_B2B =
     UnaryFnMPBall (Interval (-1.0) 1.0) $
     \x -> 1/(catchingExceptions $ 100*x^2+1)
 
+nearsingulatityFn1_PP :: Operator -> Degree -> Accuracy -> Integer -> Accuracy -> MPBall
+nearsingulatityFn1_PP OpMax _deg divThresholdAcc divIterations rangeAcc =
+    rungeMax (2.0 ^^ (- (fromAccuracy divThresholdAcc))) divIterations (fromAccuracy rangeAcc)
+nearsingulatityFn1_PP OpIntegrate _deg divThresholdAcc divIterations _rangeAcc =
+    rungeIntegral (2.0 ^^ (- (fromAccuracy divThresholdAcc))) divIterations
+
 nearsingulatityPerFn1_Name :: String
 nearsingulatityPerFn1_Name = "1/(10(sin(7x))^2+1) over [-1,1]"
 
@@ -157,6 +181,8 @@ nearsingulatityPerFn1_B2B =
     UnaryFnMPBall (Interval (-1.0) 1.0) $
     \x -> 1/(catchingExceptions $ 10*(sin (7*x))^2+1)
 
+nearsingulatityPerFn1_PP :: Operator -> Degree -> Accuracy -> Integer -> Accuracy -> MPBall
+nearsingulatityPerFn1_PP = error $ "Not yet supporting PPoly for: " ++ nearsingulatityPerFn1_Name
 
 nonsmoothFn1_Name :: String
 nonsmoothFn1_Name = "1-|x+1/3| over [-1,1]"
@@ -169,6 +195,9 @@ nonsmoothFn1_B2B :: UnaryFnMPBall
 nonsmoothFn1_B2B =
     UnaryFnMPBall (Interval (-1.0) 1.0) $
     \x -> catchingExceptions $ 1 - (abs (x+1/3))
+
+nonsmoothFn1_PP :: Operator -> Degree -> Accuracy -> Integer -> Accuracy -> MPBall
+nonsmoothFn1_PP = error $ "Not yet supporting PPoly for: " ++ nonsmoothFn1_Name
 
 nonsmoothFn2_Name :: String
 nonsmoothFn2_Name = "max(sin(10x),cos(11x)) over [-1,1]"
@@ -195,3 +224,5 @@ nonsmoothFn2_B2B =
     UnaryFnMPBall (Interval (-1.0) 1.0) $
     \x -> catchingExceptions $ max (sin (10*x)) (cos (11*x))
 
+nonsmoothFn2_PP :: Operator -> Degree -> Accuracy -> Integer -> Accuracy -> MPBall
+nonsmoothFn2_PP = error $ "Not yet supporting PPoly for: " ++ nonsmoothFn2_Name
