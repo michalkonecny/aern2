8ba19ec1345aebf841c68bf7e2fc619d57171a5f
Michal Konečný <mikkonecny@gmail.com>
2016-06-15 18:02:55 +0100

    fnreps: DFun maximisation algorithm

--- aern2-fnreps/main/fnreps-ops.hs
+++ aern2-fnreps/main/fnreps-ops.hs
@@ -4,6 +4,9 @@ import AERN2.Num
 
 import qualified Data.Map as Map
 
+import qualified Data.PQueue.Max as Q -- used in a range algorithm
+import Control.Applicative (liftA2)
+
 import System.Environment
 
 import AERN2.RealFunction
@@ -12,6 +15,7 @@ import FnReps.Polynomial.UnaryCheb.PolyBall
 import FnReps.Polynomial.UnaryCheb.Poly (compose,absX,absXshifted, reduceDegreeAndSweep)
 import qualified FnReps.PiecewisePolynomial.UnaryCheb.PPoly.Benchmarks as PPolyBench
 --import FnReps.Polynomial.UnaryCheb.RealFn
+
 
 main :: IO ()
 main =
@@ -39,6 +43,7 @@ processArgs (operationCode : functionCode : representationCode : effortArgs) =
     result = 
         case (representationCode, operationCode) of
             ("fun", "max") -> maxFun fnB2B accuracy 
+            ("dfun", "max") -> maxDFun fnB2B dfnB2B accuracy 
             ("fun", "integrate") -> integrateFun fnB2B accuracy 
             ("dfun", "integrate") -> integrateDFun fnB2B dfnB2B accuracy 
             ("poly", "max") -> maxPB $ fnPB p maxDeg1 maxDeg2
@@ -119,6 +124,52 @@ integrateDFun fn@(UnaryFnMPBall _dom f) _dfn@(UnaryFnMPBall _ df) acG =
         rB = z + r
         z = setPrecisionMatchAccuracy (ac + 100) $ mpBall 0
     
+maxDFun :: UnaryFnMPBall -> UnaryFnMPBall -> Accuracy -> MPBall
+maxDFun fn@(UnaryFnMPBall _dom f) _dfn@(UnaryFnMPBall _ df) ac =
+    head $ filter (\b -> getAccuracy b >= ac) $ filterNoException 100 True maxSequence
+    where
+    maxSequence = search fi friL $ Q.singleton $ MaxSearchSegment ri friL friR
+        where
+        (friL, friR) = gunzip $ fmap ball2endpoints fri
+        fri = fi ri
+        ri = Interval (-1.0) 1.0
+        fi i@(Interval l r) = 
+            fm + (err * (catchingExceptions $ endpoints2Ball (-o) o))
+            where
+            o = mpBall 1
+            fm = case catchingExceptions_maybeValue err of
+                    Just errV -> f (rational2BallP (getPrecision errV) m)
+                    _ -> err
+            m = (l + r)/2
+            err = (dfi i) * (r-l)/2
+        dfi = onRationalInterval df
+    search fi prevL prevQueue =
+        currentBall : 
+            search fi nextL nextQueue12
+        where
+        -- unpack the current segment and a pre-computed enclosure of the function on this segment:
+        (MaxSearchSegment seg segValL segValR, rest) = Q.deleteFindMax prevQueue
+        -- get an enclosure of the function's maximum based on previous segments and the current segment:
+        nextL 
+            | hasError prevL = segValL
+            | otherwise = liftA2 max segValL prevL
+        currentBall = liftA2 endpoints2Ball nextL segValR
+        
+        -- split the current segment and pre-compute
+        (seg1, seg2) = splitInterval seg
+        (seg1ValL, seg1ValR) = fiEE seg1
+        (seg2ValL, seg2ValR) = fiEE seg2
+        seg1NoMax = (seg1ValR <= nextL) == Just (Just True) 
+        seg2NoMax = (seg2ValR <= nextL) == Just (Just True)
+        nextQueue1 =
+            if seg1NoMax then rest else Q.insert seg1E rest
+        nextQueue12 =
+            if seg2NoMax then nextQueue1 else Q.insert seg2E nextQueue1
+        seg1E = MaxSearchSegment seg1 seg1ValL seg1ValR
+        seg2E = MaxSearchSegment seg2 seg2ValL seg2ValR
+        
+        fiEE s = 
+            gunzip $ fmap ball2endpoints $ fi s
 
 functions :: Map.Map String (String, Precision -> Degree -> Degree -> PolyBall, UnaryFnMPBall, UnaryFnMPBall, FnPP)
 functions =
