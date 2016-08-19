45f616aae2c801770c4fbcd23c7d88134af45726
Michal Konečný <mikkonecny@gmail.com>
2016-08-19 16:49:44 +0100

    CR (mixed) addition and subtraction, inc. MPBall

--- aern2-real/src/AERN2/Real/Aux.hs
+++ aern2-real/src/AERN2/Real/Aux.hs
@@ -12,8 +12,8 @@
 -}
 module AERN2.Real.Aux
 (
-  binaryOp
-  , getInitQ1Q2FromSimple
+  unaryOp, binaryOp, binaryOpWithPureArg
+  , getInitQ1FromSimple, getInitQ1TFromSimple, getInitQ1Q2FromSimple
 )
 where
 
@@ -43,6 +43,38 @@ maybeTrace
 _dummy :: ()
 _dummy = maybeTrace "dummy" ()
 
+{- generic implementations of operations of different arity -}
+
+unaryOp ::
+  (ArrowChoice to)
+  =>
+  String ->
+  (MPBall -> MPBall) ->
+  (CauchyRealA to -> (Accuracy `to` (Accuracy, Maybe MPBall))) ->
+  CauchyRealA to -> CauchyRealA to
+unaryOp name op getInitQ1 r1 = newCR name makeQ
+  where
+  makeQ =
+    proc ac ->
+      do
+      q1InitMB <- getInitQ1 r1 -< ac
+      ensureAccuracyA1 (qaMakeQuery r1) op -< (ac, q1InitMB)
+
+binaryOpWithPureArg ::
+  (ArrowChoice to)
+  =>
+  String ->
+  (MPBall -> t -> MPBall) ->
+  (CauchyRealA to -> t -> (Accuracy `to` (Accuracy, Maybe MPBall))) ->
+  CauchyRealA to -> t -> CauchyRealA to
+binaryOpWithPureArg name op getInitQ1T r1 t = newCR name makeQ
+  where
+  makeQ =
+    proc ac ->
+      do
+      q1InitMB <- getInitQ1T r1 t -< ac
+      ensureAccuracyA1 (qaMakeQuery r1) (flip op t) -< (ac, q1InitMB)
+
 binaryOp ::
   (ArrowChoice to)
   =>
@@ -57,6 +89,87 @@ binaryOp name op getInitQ1Q2 r1 r2 = newCR name makeQ
       do
       (q1InitMB, q2InitMB) <- getInitQ1Q2 r1 r2 -< ac
       ensureAccuracyA2 (qaMakeQuery r1) (qaMakeQuery r2) op -< (ac, q1InitMB, q2InitMB)
+
+{- functions to help determine initial queries -}
+
+getInitQ1FromSimple ::
+  (Arrow to)
+  =>
+  Accuracy `to` q ->
+  r1 -> Accuracy `to` (q, Maybe MPBall)
+getInitQ1FromSimple simpleA _ =
+  proc q ->
+    do
+    initQ1 <- simpleA -< q
+    returnA -< (initQ1, Nothing)
+
+getInitQ1TFromSimple ::
+  (Arrow to)
+  =>
+  Accuracy `to` q ->
+  r1 -> t -> Accuracy `to` (q, Maybe MPBall)
+getInitQ1TFromSimple simpleA _ _ =
+  proc q ->
+    do
+    initQ1 <- simpleA -< q
+    returnA -< (initQ1, Nothing)
+
+getInitQ1Q2FromSimple ::
+  (Arrow to)
+  =>
+  Accuracy `to` (q,q) ->
+  r1 -> r2 -> Accuracy `to` ((q, Maybe MPBall), (q, Maybe MPBall))
+getInitQ1Q2FromSimple simpleA _ _ =
+  proc q ->
+    do
+    (initQ1, initQ2) <- simpleA -< q
+    returnA -< ((initQ1, Nothing), (initQ2, Nothing))
+
+{-
+  functions for iterative querying of operands
+  until the result is of a sufficient accuracy
+-}
+
+ensureAccuracyA1 ::
+  (ArrowChoice to) =>
+  (Accuracy `to` MPBall) ->
+  (MPBall -> MPBall) ->
+  ((Accuracy, (Accuracy, Maybe MPBall)) `to` MPBall)
+ensureAccuracyA1 getA1 op =
+    proc (q,(j1, mB)) ->
+        do
+        let mResult = fmap op mB
+        case mResult of
+            Just result | getAccuracy result >= q ->
+                returnA -<
+                    maybeTrace (
+                        "ensureAccuracy1: Pre-computed result sufficient. (q = " ++ show q ++
+                        "; j1 = " ++ show j1 ++
+                        "; result accuracy = " ++ (show $ getAccuracy result) ++ ")"
+                    ) $
+                    result
+            _ -> aux -< (q,j1)
+    where
+    aux =
+        proc (q,j1) ->
+            do
+            a1 <- getA1 -< j1
+            let result = op a1
+            if getAccuracy result >= q
+                then returnA -<
+                    maybeTrace (
+                        "ensureAccuracy1: Succeeded. (q = " ++ show q ++
+                        "; j1 = " ++ show j1 ++
+                        "; result accuracy = " ++ (show $ getAccuracy result) ++ ")"
+                    ) $
+                    result
+                else aux -<
+                    maybeTrace (
+                        "ensureAccuracy1: Not enough ... (q = " ++ show q ++
+                        "; j1 = " ++ show j1 ++
+                        "; result accuracy = " ++ (show $ getAccuracy result) ++ ")"
+                    ) $
+                    (q, j1+1)
 
 ensureAccuracyA2 ::
   (ArrowChoice to) =>
@@ -108,14 +221,3 @@ ensureAccuracyA2 getA1 getA2 op =
                         "; result accuracy = " ++ (show $ getAccuracy result) ++ ")"
                     ) $
                     (q,j1+1,j2+1)
-
-getInitQ1Q2FromSimple ::
-  (Arrow to)
-  =>
-  Accuracy `to` (q,q) ->
-  r1 -> r2 -> Accuracy `to` ((q, Maybe MPBall), (q, Maybe MPBall))
-getInitQ1Q2FromSimple simpleA _ _ =
-  proc q ->
-    do
-    (initQ1, initQ2) <- simpleA -< q
-    returnA -< ((initQ1, Nothing), (initQ2, Nothing))
--- aern2-real/src/AERN2/Real/Field.hs
+++ aern2-real/src/AERN2/Real/Field.hs
@@ -15,9 +15,10 @@ module AERN2.Real.Field
 )
 where
 
-import Numeric.MixedTypes
+import Numeric.MixedTypes hiding (id)
 -- import qualified Prelude as P
 
+import Control.Category (id)
 import Control.Arrow
 
 import AERN2.MP.Ball
@@ -42,10 +43,6 @@ maybeTrace
 _dummy :: ()
 _dummy = maybeTrace "dummy" ()
 
-
-instance (QAArrow to) => CanAddAsymmetric (CauchyRealA to) (CauchyRealA to) where
-  type AddType (CauchyRealA to) (CauchyRealA to) = (CauchyRealA to)
-  add = binaryOp "+" add (getInitQ1Q2FromSimple $ proc q -> returnA -< (q,q))
 
 {-|
   Example arrow-generic real number computation
@@ -82,3 +79,71 @@ _addAcachedPrint =
       do
       x <-_addAcached -< ()
       qaMakeQueryA -< (x, bits 10)
+
+{- negation -}
+
+instance (ArrowChoice to) => CanNeg (CauchyRealA to) where
+  negate = unaryOp "-" negate (getInitQ1FromSimple $ proc q -> returnA -< q)
+
+{- addition -}
+
+instance (ArrowChoice to) => CanAddAsymmetric (CauchyRealA to) (CauchyRealA to) where
+  add = binaryOp "+" add (getInitQ1Q2FromSimple $ proc q -> returnA -< (q,q))
+
+instance (ArrowChoice to) => CanAddAsymmetric (CauchyRealA to) Integer where
+  type AddType (CauchyRealA to) Integer = CauchyRealA to
+  add = binaryOpWithPureArg "+" add (getInitQ1TFromSimple id)
+
+instance (ArrowChoice to) => CanAddAsymmetric Integer (CauchyRealA to) where
+  type AddType Integer (CauchyRealA to) = CauchyRealA to
+  add = flip add
+
+instance (ArrowChoice to) => CanAddAsymmetric (CauchyRealA to) Int where
+  type AddType (CauchyRealA to) Int = CauchyRealA to
+  add = binaryOpWithPureArg "+" add (getInitQ1TFromSimple id)
+
+instance (ArrowChoice to) => CanAddAsymmetric Int (CauchyRealA to) where
+  type AddType Int (CauchyRealA to) = CauchyRealA to
+  add = flip add
+
+instance (ArrowChoice to) => CanAddAsymmetric (CauchyRealA to) Rational where
+  type AddType (CauchyRealA to) Rational = CauchyRealA to
+  add = binaryOpWithPureArg "+" add (getInitQ1TFromSimple id)
+
+instance (ArrowChoice to) => CanAddAsymmetric Rational (CauchyRealA to) where
+  type AddType Rational (CauchyRealA to) = CauchyRealA to
+  add = flip add
+
+{- MPBall + CauchyReal = MPBall, only allowed in the (->) arrow  -}
+
+mpBallSimilarTo :: MPBall -> CauchyReal -> MPBall
+mpBallSimilarTo b r =
+  qaMakeQuery r $ getAccuracyIfExactUsePrec b
+
+getAccuracyIfExactUsePrec :: MPBall -> Accuracy
+getAccuracyIfExactUsePrec ball =
+  case getAccuracy ball of
+    Exact -> bits (getPrecision ball)
+    result -> result
+
+instance CanAddAsymmetric CauchyReal MPBall where
+  type AddType CauchyReal MPBall = MPBall
+  add r b = add (mpBallSimilarTo b r) b
+
+instance CanAddAsymmetric MPBall CauchyReal where
+  type AddType MPBall CauchyReal = MPBall
+  add = flip add
+
+{- subtraction -}
+
+instance (ArrowChoice to) => CanSub (CauchyRealA to) (CauchyRealA to) where
+  sub = binaryOp "-" sub (getInitQ1Q2FromSimple $ proc q -> returnA -< (q,q))
+
+instance (ArrowChoice to) => CanSub (CauchyRealA to) Integer
+instance (ArrowChoice to) => CanSub Integer (CauchyRealA to)
+instance (ArrowChoice to) => CanSub (CauchyRealA to) Int
+instance (ArrowChoice to) => CanSub Int (CauchyRealA to)
+instance (ArrowChoice to) => CanSub (CauchyRealA to) Rational
+instance (ArrowChoice to) => CanSub Rational (CauchyRealA to)
+instance CanSub CauchyReal MPBall
+instance CanSub MPBall CauchyReal
