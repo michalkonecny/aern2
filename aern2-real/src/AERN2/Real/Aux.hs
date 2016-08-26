bf206bdc9d2452cfb60d8bb27af7cfc1b45e83e3
Michal Konečný <mikkonecny@gmail.com>
2016-08-26 14:18:54 +0100

    disable debugging for CR operations

--- aern2-real/src/AERN2/Real/Aux.hs
+++ aern2-real/src/AERN2/Real/Aux.hs
@@ -36,8 +36,8 @@ import AERN2.Real.Type
 import Debug.Trace (trace)
 
 shouldTrace :: Bool
--- shouldTrace = False
-shouldTrace = True
+shouldTrace = False
+-- shouldTrace = True
 
 maybeTrace :: String -> a -> a
 maybeTrace
