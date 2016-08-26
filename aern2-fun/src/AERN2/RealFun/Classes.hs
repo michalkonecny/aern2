66fdc5da37d3f1818f4e3b37c119c4bbe38318d5
Michal Konečný <mikkonecny@gmail.com>
2016-08-26 16:32:39 +0100

    document aern2-fun class functions

--- aern2-fun/src/AERN2/RealFun/Classes.hs
+++ aern2-fun/src/AERN2/RealFun/Classes.hs
@@ -36,12 +36,17 @@ class HasDomain f where
   type Domain f
   getDomain :: f -> Domain f
 
-class CanApply f d where
-  type ApplyType f d
-  apply :: f -> d -> ApplyType f d
+class CanApply f x where
+  type ApplyType f x
+  {-| compute @f(x)@  -}
+  apply :: f {-^ @f@ -} -> x {-^ @x@ -} -> ApplyType f x
 
 class HasVars f where
   type Var f
-  varF :: Var f -> f
+  {-| the function @x@, ie the function that project the domain to the given variable @x@  -}
+  varF ::
+    f {-^ sample function with the same domain -}->
+    Var f {-^ @x@ -} ->
+    f
 
 type HasConstFunctions t f = ConvertibleExactly t f
