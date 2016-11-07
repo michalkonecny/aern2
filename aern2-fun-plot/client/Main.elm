27636f3f81622a21decdee42926f21648149926a
Michal Konečný <mikkonecny@gmail.com>
2016-11-07 11:49:58 +0000

    Elm plot client showing big boxes

--- aern2-fun-plot/client/Main.elm
+++ aern2-fun-plot/client/Main.elm
@@ -6,14 +6,19 @@ import List
 
 import Html exposing (..)
 import Html.App exposing (program)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+-- import Html.Attributes exposing (..)
+-- import Html.Events exposing (..)
+
+import Color exposing (..)
+import Collage exposing (..)
+import Element exposing (..)
+import Text
 
 import Http
 import Task exposing (Task, andThen)
 
 import Api exposing (..)
-import DInterval exposing (divDi, mulDi)
+import DInterval exposing (divDi, mulDi, dToFloat)
 
 main : Program Never
 main =
@@ -38,22 +43,53 @@ type alias FunctionDetails =
 type alias State =
     { functionIds : List FunctionId
     , functionDetails : Dict FunctionId FunctionDetails
-    , functionPoints : Dict FunctionId (List FunctionPoint)
-    , plotDomain : Maybe DyadicIntervalAPI
-    , plotArea : { w : Pixels, h : Pixels }
+    , functionPoints : Dict FunctionId (List FunctionPointF)
+    , plotArea : Maybe PlotArea
+    , plotCanvasSize : { w : Pixels, h : Pixels }
     , plotResolution : Pixels
     , error : Maybe String
     }
 
+type alias FunctionPointF =
+  { domL : Float
+  , domR : Float
+  , valL : (Float, Float)
+  , valR : (Float, Float)
+  }
+
+functionPointF : FunctionPoint -> FunctionPointF
+functionPointF pt =
+  { domL = dToFloat pt.functionPointDom.dyadic_endpointL
+  , domR = dToFloat pt.functionPointDom.dyadic_endpointR
+  , valL = mpBallToFloatFloat pt.functionPointValue.mpBall_endpointL
+  , valR = mpBallToFloatFloat pt.functionPointValue.mpBall_endpointR
+  }
+
+mpBallToFloatFloat : MPBall -> (Float, Float)
+mpBallToFloatFloat b =
+  let
+    c = dToFloat b.ball_value
+    e = dToFloat b.ball_error
+  in (c - e, c + e)
+
+type alias PlotArea =
+  { domain : DyadicIntervalAPI
+  , domL : Float
+  , domR : Float
+  , rangeL : Float
+  , rangeR : Float
+  }
+
 type alias Pixels = Int
+
 
 initState : State
 initState =
   { functionIds = []
   , functionDetails = Dict.empty
   , functionPoints = Dict.empty
-  , plotDomain = Nothing
-  , plotArea = { w = 800, h = 800 }
+  , plotArea = Nothing
+  , plotCanvasSize = { w = 800, h = 800 }
   , plotResolution = 100 -- TODO: change to 2 pixels
   , error = Nothing
   }
@@ -88,14 +124,21 @@ getFunctionsPoints s (fnIds, fnDetails) =
   let
     domains = List.map (.domain) (Dict.values fnDetails)
     plotDomain = DInterval.unions domains
-    maxStep = ((DInterval.width plotDomain) `mulDi` s.plotResolution) `divDi` s.plotArea.w
+    maxStep = ((DInterval.width plotDomain) `mulDi` s.plotResolution) `divDi` s.plotCanvasSize.w
     sampling = { sampling_dom' = plotDomain, sampling_maxStep = maxStep }
+    plotArea =
+      { domain = plotDomain
+      , domL = dToFloat plotDomain.dyadic_endpointL
+      , domR = dToFloat plotDomain.dyadic_endpointR
+      , rangeL = -1.0 -- TODO
+      , rangeR = 1.0
+      }
     makeState fnPoints =
       { initState |
         functionIds = fnIds
       , functionDetails = fnDetails
-      , functionPoints = fnPoints
-      , plotDomain = Just plotDomain }
+      , functionPoints = Dict.map (\ _ -> List.map functionPointF) fnPoints
+      , plotArea = Just plotArea }
   in
   Task.map makeState <|
     postApiSampling sampling
@@ -146,5 +189,63 @@ update msg s =
 
 view : State -> Html Msg
 view s =
-  div []
-    <| [text (toString s)]
+  let
+    width = s.plotCanvasSize.w
+    height = s.plotCanvasSize.h
+  in
+    toHtml <|
+    container width height middle <|
+    collage width height <|
+      [ rect (toFloat width) (toFloat height)
+          |> filled bgrColour
+      ] ++
+      (List.concat <| List.map (drawFn s) <| Dict.toList s.functionPoints)
+
+drawFn s (fnId, points) =
+  List.concat <| List.map (drawPoint s) points
+
+drawPoint s point =
+  case s.plotArea of
+    Nothing -> []
+    Just plotArea ->
+      let
+        w = toFloat s.plotCanvasSize.w
+        h = toFloat s.plotCanvasSize.h
+        pAdL = plotArea.domL
+        pAdR = plotArea.domR
+        pArL = plotArea.rangeL
+        pArR = plotArea.rangeR
+        toCoordX x = (w/2) * (2*x - pAdL - pAdR) / (pAdR - pAdL)
+        toCoordY y = (h/2) * (2*y - pArL - pArR) / (pArR - pArL)
+        domL = toCoordX point.domL
+        domR = toCoordX point.domR
+        domM = (domL + domR) / 2
+        domW = domR - domL
+        vLL = toCoordY <| fst point.valL
+        vLR = toCoordY <| snd point.valL
+        vLc = (vLL + vLR)/2
+        vLd = vLR - vLL
+        vRL = toCoordY <| fst point.valR
+        vRR = toCoordY <| snd point.valR
+        vRc = (vRL + vRR)/2
+        vRd = vRR - vRL
+      in
+      [
+        rect domW vLd |> filled enclosureFillColour |> move (domM, vLc)
+      , rect domW vLd |> outlined enclosureOutlineStyle |> move (domM, vLc)
+      , rect domW vRd |> filled enclosureFillColour |> move (domM, vRc)
+      , rect domW vRd |> outlined enclosureOutlineStyle |> move (domM, vRc)
+      ]
+
+bgrColour =
+  -- white
+  rgb 200 255 200
+
+enclosureFillColour =
+  rgb 200 200 255
+
+enclosureOutlineStyle : LineStyle
+enclosureOutlineStyle =
+  { defaultLine |
+    color = rgb 10 10 255
+  }
