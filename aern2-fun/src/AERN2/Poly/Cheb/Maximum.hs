ab9aa5a51966221e385b5e1e52697640ea9af913
Michal Konecny <mikkonecny@gmail.com>
2016-11-23 21:07:05 +0000

    Simplify import of Poly.Power in Cheb.Maximum

--- aern2-fun/src/AERN2/Poly/Cheb/Maximum.hs
+++ aern2-fun/src/AERN2/Poly/Cheb/Maximum.hs
@@ -3,19 +3,19 @@ module AERN2.Poly.Cheb.Maximum where
 
 import Numeric.MixedTypes
 import AERN2.MP.Ball
-import AERN2.MP.Dyadic
+-- import AERN2.MP.Dyadic
 import qualified Data.Map as Map
 
+import qualified AERN2.Poly.Power as Pow
+
 import AERN2.Poly.Cheb.Type
-import qualified AERN2.Poly.Power.Type as Pow
 import AERN2.Poly.Cheb.Eval
 import AERN2.Poly.Conversion
-import qualified AERN2.Poly.Power.Maximum as PMax
 import AERN2.Interval
 
 maximum :: ChPoly MPBall -> MPBall -> MPBall -> MPBall
 maximum (ChPoly dom poly) l r  =
-   PMax.genericMaximum (evalDf f f') (Map.fromList [(0, f')])
+   Pow.genericMaximum (evalDf f f') (Map.fromList [(0, f')])
     (fromDomToUnitInterval dom l) (fromDomToUnitInterval dom r)
    where
    f  = ChPoly (dyadicInterval (-1,1)) poly
@@ -23,7 +23,7 @@ maximum (ChPoly dom poly) l r  =
 
 maximumOptimised :: ChPoly MPBall -> MPBall -> MPBall -> Integer -> Integer -> MPBall
 maximumOptimised f l r initialDegree steps =
-  PMax.maximumOptimised (cheb2Power $ chPoly_poly f) l r initialDegree steps  -- TODO: evaluate in Chebyshev basis - this requires a good evaluation function
+  Pow.maximumOptimised (cheb2Power $ chPoly_poly f) l r initialDegree steps  -- TODO: evaluate in Chebyshev basis - this requires a good evaluation function
 
 {-minimumI :: ChPoly MPBall -> MPBall
 minimumI f = -maximumI (-f)-}
