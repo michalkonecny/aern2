95ecd7e0a3d3a38d5b65694c63ef7ce212a078ff
Michal Konecny <mikkonecny@gmail.com>
2016-12-06 14:19:56 +0000

    new module forgotten in prev commit

---
+++ aern2-fun/src/AERN2/PPoly.hs
@@ -0,0 +1,46 @@
+{-|
+    Module      :  AERN2.PPoly
+    Description :  Unary piece-wise sparse polynomials
+    Copyright   :  (c) Eike Neumann, Michal Konecny
+    License     :  BSD3
+
+    Maintainer  :  mikkonecny@gmail.com
+    Stability   :  experimental
+    Portability :  portable
+
+    Unary piece-wise sparse polynomials
+-}
+
+module AERN2.PPoly
+(
+  module AERN2.PPoly.Type
+, module AERN2.PPoly.Division
+, module AERN2.PPoly.Maximum
+, module AERN2.PPoly.Eval
+)
+where
+
+-- import Numeric.MixedTypes
+-- import qualified Prelude as P
+-- import Text.Printf
+
+-- import qualified Data.List as List
+-- import qualified Data.Map as Map
+
+-- import Test.Hspec
+-- import Test.QuickCheck
+
+-- import AERN2.MP.ErrorBound
+-- import AERN2.MP.Ball
+-- import AERN2.MP.Dyadic
+--
+-- import AERN2.Real
+--
+-- import AERN2.Interval
+-- import AERN2.RealFun.Operations
+-- import AERN2.RealFun.UnaryFun
+
+import AERN2.PPoly.Type
+import AERN2.PPoly.Division
+import AERN2.PPoly.Maximum
+import AERN2.PPoly.Eval
