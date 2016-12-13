f9bd68844142b6429016d1c0902a3e16ec3e727f
mikkonecny <mikkonecny@gmail.com>
2016-12-13 10:54:17 +0000

    plotting excutable: can choose function set

--- aern2-fun-plot/example/Main.hs
+++ aern2-fun-plot/example/Main.hs
@@ -1,6 +1,8 @@
 import Numeric.MixedTypes
 -- import qualified Prelude as P
 
+import System.Environment
+
 import AERN2.MP.Dyadic
 import AERN2.MP
 
@@ -13,43 +15,68 @@ import AERN2.Poly.Cheb (ChPoly)
 import AERN2.RealFun.PlotService as Plot
 
 main :: IO ()
-main = Plot.startServer fns shouldLog (int 4000)
+main =
+  do
+  args <- getArgs
+  Plot.startServer (fns args) shouldLog (int 4000)
   where
   shouldLog = True
 
-fns :: Plot.Functions
-fns = fnsPoly ++ fnsFun
-
-fnsFun :: Plot.Functions
-fnsFun = map funFn
-  [
-    ("Fun 0", unitDom, \x -> x-x)
-  , ("Fun 1/(10*x^2+1)", unitDom, \x -> 1/(10*x^2+1))
-  -- , ("Fun (1+sin(6x))/2", unitDom, \x -> (1+sin (6*x))/2)
-  -- , ("Fun (1+cos(6x))/2", unitDom, \x -> (1+cos (6*x))/2)
-  ]
+fns :: [String] -> Plot.Functions
+fns ["runge"] = fnsRunge
+fns ["sine"] = fnsSine
+fns ["square"] = fnsSquare
+fns _ = error "Please specify what to plot."
 
 unitDom :: DyadicInterval
 unitDom = dyadicInterval (-1.0,1.0)
 unitDomP :: DyadicInterval
 unitDomP = dyadicInterval (0.0,1.0)
 
-fnsPoly :: Plot.Functions
-fnsPoly = map chPolyFn
-  [
-    ("Poly 0", x - x)
-  , ("Poly 1/(10*x^2+1)", ChPoly.reduceDegree 8 $ ChPoly.chebDivideDCT (bits 4) (x-x+1) (10*x*x+1))
-  , ("Poly 1/(10*x^2+1)", ChPoly.reduceDegree 12 $ ChPoly.chebDivideDCT (bits 4) (x-x+1) (10*x*x+1))
-  , ("Poly 1/(10*x^2+1)", ChPoly.reduceDegree 16 $ ChPoly.chebDivideDCT (bits 4) (x-x+1) (10*x*x+1))
-  -- , ("Poly (1+sin[ac=3](6x))/2", (1+sine (6*x))/2)
-  -- , ("Poly (1+cos[ac=3](6x))/2", (1+cosine (6*x))/2)
+fnsRunge :: Plot.Functions
+fnsRunge =
+  map chPolyFn
+  [ ("Poly(8) 1/(10*x^2+1)", ChPoly.reduceDegree 8 runge_4bits)
+  , ("Poly(12) 1/(10*x^2+1)", ChPoly.reduceDegree 12 runge_4bits)
+  , ("Poly(16) 1/(10*x^2+1)", ChPoly.reduceDegree 16 runge_4bits)
   ]
+  ++
+  (map funFn
+  [ ("Fun 1/(10*x^2+1)", unitDom, \x -> 1/(10*x^2+1))])
   where
-  -- sine = sineWithAccuracyGuide (bits 3)
-  -- cosine = cosineWithAccuracyGuide (bits 3)
-  x :: ChPoly MPBall
-  x = varFn sampleFn ()
+  runge_4bits = ChPoly.chebDivideDCT (bits 4) (xU-xU+1) (10*xU*xU+1)
+  xU :: ChPoly MPBall
+  xU = varFn sampleFn ()
   sampleFn = constFn (unitDom, 1)
+
+fnsSine :: Plot.Functions
+fnsSine =
+  map chPolyFn
+  [ ("Poly (1+sin[ac=3](6x))/2", (1+sine (6*xU))/2)
+  , ("Poly (1+cos[ac=3](6x))/2", (1+cosine (6*xU))/2)
+  ]
+  ++
+  (map funFn
+  [ ("Fun (1+sin(6x))/2", unitDom, \x -> (1+sin (6*x))/2)
+  , ("Fun (1+cos(6x))/2", unitDom, \x -> (1+cos (6*x))/2)
+  ])
+  where
+  sine = sineWithAccuracyGuide (bits 3)
+  cosine = cosineWithAccuracyGuide (bits 3)
+  xU :: ChPoly MPBall
+  xU = varFn sampleFn ()
+  sampleFn = constFn (unitDom, 1)
+
+fnsSquare :: Plot.Functions
+fnsSquare =
+  map chPolyFn
+  [ ("Poly x^2", ChPoly.reduceDegree 1 $ xP*xP) ]
+  ++
+  (map funFn
+  [
+    ("Fun x^2", unitDom, \x -> x^2)
+  ])
+  where
   xP :: ChPoly MPBall
   xP = varFn sampleFnP ()
   sampleFnP = constFn (unitDomP, 1)
