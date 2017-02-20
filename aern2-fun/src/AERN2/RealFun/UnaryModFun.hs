08ea9f6084a2b2380eb0e394c6f690e7f486df8e
mikkonecny <mikkonecny@gmail.com>
2017-02-20 21:13:46 +0000

    UnaryModFun maximisation & integration vai UnaryBallFun

--- aern2-fun/src/AERN2/RealFun/UnaryModFun.hs
+++ aern2-fun/src/AERN2/RealFun/UnaryModFun.hs
@@ -25,14 +25,14 @@ import Numeric.MixedTypes
 
 import Numeric.CatchingExceptions
 
-import AERN2.Norm
+-- import AERN2.Norm
 import AERN2.MP
 -- import qualified AERN2.MP.Ball as MPBall
 import AERN2.MP.Dyadic
 
 import AERN2.Real
-import AERN2.Interval (Interval(..), DyadicInterval)
-import qualified AERN2.Interval as Interval
+import AERN2.Interval (DyadicInterval)
+-- import qualified AERN2.Interval as Interval
 
 import AERN2.RealFun.Operations
 
@@ -96,63 +96,19 @@ instance ConvertibleExactly UnaryModFun UnaryBallFun where
         rangeOnPoints = fromEndpoints (qaMakeQuery minVal ac) (qaMakeQuery maxVal ac)
         ac = 1 + getAccuracy b
 
-
 -- instance CanApply UnaryBallDFun DyadicInterval where
 --   type ApplyType UnaryBallDFun DyadicInterval = RealInterval
 --   apply f di =
 --     Interval (minimumOverDom f di) (maximumOverDom f di)
 --
--- instance CanMaximiseOverDom UnaryBallDFun DyadicInterval where
---   type MaximumOverDomType UnaryBallDFun DyadicInterval = CauchyReal
---   maximumOverDom (UnaryBallDFun []) _ = error "maximumOverDom UnaryBallDFun []"
---   maximumOverDom (UnaryBallDFun (UnaryBallFun _ f_o : derivatives_o)) di_o =
---     maximumOnIntervalSubdivide (evalUseD derivatives_o f_o) di_o
---
--- instance CanMinimiseOverDom UnaryBallDFun DyadicInterval where
---   type MinimumOverDomType UnaryBallDFun DyadicInterval = CauchyReal
---   minimumOverDom (UnaryBallDFun []) _ = error "minimumOverDom UnaryBallDFun []"
---   minimumOverDom (UnaryBallDFun (UnaryBallFun _ f_o : derivatives_o)) di_o =
---     minimumOnIntervalSubdivide (evalUseD derivatives_o f_o) di_o
---
--- evalUseD ::
---   [UnaryBallFun] ->
---   (CatchingNumExceptions MPBall -> CatchingNumExceptions MPBall) ->
---   DyadicInterval ->
---   (Maybe (CatchingNumExceptions MPBall, CatchingNumExceptions MPBall), CatchingNumExceptions MPBall)
--- evalUseD [] f di = (Nothing, evalOnIntervalGuessPrecision f di)
--- evalUseD (UnaryBallFun _ f' : rest) f di@(Interval l r)
---   | f'di !>=! 0 = (Just (fl,fr), liftA2 fromEndpoints fl fr)
---   | f'di !<=! 0 = (Just (fr,fl), liftA2 fromEndpoints fr fl)
---   | otherwise = (Nothing, fm + errBall)
---   where
---   (_, f'di) = evalUseD rest f' di -- recursive call
---   fl = f $ catchingNumExceptions $ raisePrecisionIfBelow p $ mpBall l
---   fr = f $ catchingNumExceptions $ raisePrecisionIfBelow p $ mpBall r
---   fm = f $ catchingNumExceptions $ raisePrecisionIfBelow p $ mpBall m
---   m = (l + r)*half
---   errBall = f'di*((r-l)*half)*unitBall
---   unitBall = catchingNumExceptions $ mpBall (-1,1)
---   half = dyadic 0.5
---
---   p =
---       case nl of
---           NormBits i -> prec $ max 10 (-i)
---           NormZero -> getPrecision l
---   nl = getNormLog (r - l)
---
---
--- instance CanIntegrateOverDom UnaryBallDFun DyadicInterval where
---   type IntegralOverDomType UnaryBallDFun DyadicInterval = CauchyReal
---   integrateOverDom (UnaryBallDFun []) = error "integrating UnaryBallDFun []"
---   integrateOverDom (UnaryBallDFun [f]) = integrateOverDom f
---   integrateOverDom (UnaryBallDFun (f : f' : _)) =
---     integralOnIntervalSubdivide (integralOnIntervalIncreasePrecision getArea) standardPrecisions
---     where
---     getArea di p =
---       (apply f diM)*diW+errB
---       where
---       diW = Interval.width di
---       errB = ((deriv - deriv)/2)*((diW*0.5)^2)*0.5
---       deriv = apply f' (catchingNumExceptions diB)
---       diM = catchingNumExceptions $ centreAsBall diB
---       diB = setPrecision p $ mpBall di
+instance CanMaximiseOverDom UnaryModFun DyadicInterval where
+  type MaximumOverDomType UnaryModFun DyadicInterval = CauchyReal
+  maximumOverDom = maximumOverDom . unaryBallFun
+
+instance CanMinimiseOverDom UnaryModFun DyadicInterval where
+  type MinimumOverDomType UnaryModFun DyadicInterval = CauchyReal
+  minimumOverDom = minimumOverDom . unaryBallFun
+
+instance CanIntegrateOverDom UnaryModFun DyadicInterval where
+  type IntegralOverDomType UnaryModFun DyadicInterval = CauchyReal
+  integrateOverDom = integrateOverDom . unaryBallFun
