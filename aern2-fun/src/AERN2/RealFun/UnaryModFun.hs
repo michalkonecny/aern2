f5fb86469f581e454eb7f6acdd7c9e451af4cd43
mikkonecny <mikkonecny@gmail.com>
2017-02-21 10:40:47 +0000

    UnaryModFun division

--- aern2-fun/src/AERN2/RealFun/UnaryModFun.hs
+++ aern2-fun/src/AERN2/RealFun/UnaryModFun.hs
@@ -152,24 +152,6 @@ instance CanIntegrateOverDom UnaryModFun DyadicInterval where
 
 {- selected field ops -}
 
-example_ModFun :: UnaryModFun
-example_ModFun =
-  -- sin(10*x)+cos(20*x)
-  x*(x+1)
-  where
-  x = varFn (unaryModFun (unaryIntervalDom, 0)) ()
-
-mtest1 :: Integer -> MPBall
-mtest1 b =
-  qaMakeQuery m (bits b)
-  where
-  m =
-    minimumOverDom example_ModFun $
-      dyadicInterval (-1.0,1.0)
-
-unaryIntervalDom :: DyadicInterval
-unaryIntervalDom = dyadicInterval (-1,1)
-
 instance CanAddAsymmetric UnaryModFun UnaryModFun where
   add (UnaryModFun dom1 eval1 modulus1) (UnaryModFun dom2 eval2 modulus2)
     | dom1 == dom2 =
@@ -193,7 +175,7 @@ instance CanMulAsymmetric UnaryModFun UnaryModFun where
     | dom1 == dom2 =
       UnaryModFun dom1 (\d -> eval1 d * eval2 d) modulus'
     | otherwise =
-      error "UnaryModFun: add: incompatible domains"
+      error "UnaryModFun: mul: incompatible domains"
     where
     modulus' b i = max m1 m2
       where
@@ -222,3 +204,64 @@ instance CanMulAsymmetric Integer UnaryModFun where
 instance CanMulAsymmetric UnaryModFun Integer where
   type MulType UnaryModFun Integer = UnaryModFun
   mul f n = mul n f
+
+instance CanDiv UnaryModFun UnaryModFun where
+  divide f1 f2 = f1 * recipF f2
+    where
+    recipF f@(UnaryModFun dom eval modulus) =
+      UnaryModFun dom eval' modulus'
+      where
+      eval' d =
+        do
+        fd <- eval d
+        pure $ 1 / fd -- cannot detect div by 0 for CauchyReals...
+      modulus' b i =
+        max (-m) ((modulus b i) + 1 - 2*m)
+        where
+        m = splitting maxDepth b
+        maxDepth = 20
+        splitting md b0
+          | md == 0 = error "UnaryModFun: division modulus: division by zero"
+          | otherwise =
+            case mOnB b0 of
+              Nothing ->
+                max (splitting (md-1) b1) (splitting (md-1) b2)
+              Just m0 -> m0
+            where
+            b1 = mpBall b1I
+            b2 = mpBall b2I
+            (b1I,b2I) = split $ dyadicInterval b
+      mOnB b
+        | fb !>! 0 = Just m
+        | otherwise = Nothing
+        where
+        fb = abs $ apply (unaryBallFun f) b
+        m =
+          case getNormLog (1/fb) of
+            NormZero -> constFnModulus
+            NormBits nfb -> -nfb
+
+instance CanDiv Integer UnaryModFun where
+  type DivType Integer UnaryModFun = UnaryModFun
+  divide n f = divide (unaryModFun (getDomain f, n)) f
+
+example_ModFun :: UnaryModFun
+example_ModFun =
+  -- sin(10*x)+cos(20*x)
+  1/((10*x*x)+1)
+  -- 1/(x+2)
+  where
+  x = varFn (unaryModFun (unaryIntervalDom, 0)) ()
+
+mtest1 :: Integer -> MPBall
+mtest1 b =
+  qaMakeQuery m (bits b)
+  where
+  m =
+    -- integrateOverDom example_ModFun $
+    -- minimumOverDom example_ModFun $
+    maximumOverDom example_ModFun $
+      dyadicInterval (-1.0,1.0)
+
+unaryIntervalDom :: DyadicInterval
+unaryIntervalDom = dyadicInterval (-1,1)
