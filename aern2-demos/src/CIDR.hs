e624892f13227c49e8a344b9a1664ca934b4816c
mikkonecny <mikkonecny@gmail.com>
2017-08-17 14:25:50 +0100

    CIDR : Improved lim, added lim with Lipschitz - inspired by discussion with Norbert Mueller

--- aern2-demos/src/CIDR.hs
+++ aern2-demos/src/CIDR.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
 {-# LANGUAGE CPP #-}
-#define DEBUG
+-- #define DEBUG
 {-|
 
 Experimenting with programming in CIDR shallow
@@ -28,48 +29,87 @@ import MixedTypesNumPrelude
 import Control.CollectErrors
 
 import AERN2.MP.Precision
+import AERN2.MP.Dyadic
 
 import AERN2.QA.Protocol
 import AERN2.Sequence
 import AERN2.Real
 
+------------------------------------------------------
+-- sqrt example using limit WITHOUT Lipschitz info
+------------------------------------------------------
+
 {-|
   @sqrtApprox k x@
 
-  preconditions: @x > 0@,  @k >= 0@
+  preconditions: @x > 1@,  @k >= 0@
 
   returns a real number that is within
   $2^{{}-k}$ away from $\sqrt{x}$.
 -}
 sqrtApprox ::
-  (OrderedField r
-  , CanAbsSameType r
-  , CanTestMVIsPositiveUpTo r
-  , HasBools (MVIsPositiveUpToType r)
-  , CanChoose (MVIsPositiveUpToType r) (EnsureCN r)
-  -- , CanChoose (OrderCompareType Rational r) (EnsureCN r)
-  , Show r)
-  =>
-  Integer -> r -> EnsureCN r
+  _ => Integer -> r -> EnsureCN r
 sqrtApprox p x =
   while x isAccurate step
   where
   isAccurate y = mvIsPositiveUpTo (p+1) (0.5^!(p+1) - abs(y*y-x))
-  -- isAccurate y = 0.5^!(p) >= abs(y*y-x)
   step y = (y + (x/!y))/!2
 
+{-|
+  @mysqrt x@
+
+  preconditions: @x > 1@
+
+  returns $\sqrt{x}$.
+-}
 mysqrt ::
-  (OrderedField r
-  , CanAbsSameType r
-  , CanTestMVIsPositiveUpTo r
-  , HasBools (MVIsPositiveUpToType r)
-  , CanChoose (MVIsPositiveUpToType r) (EnsureCN r)
-  -- , CanChoose (OrderCompareType Rational r) (EnsureCN r)
-  , HasLimits Accuracy (r -> (EnsureCN r))
-  , Show r)
-  =>
-  r -> EnsureCN r
-mysqrt = limit sqrtApprox
+  _ => r -> EnsureCN r
+mysqrt (x :: r) = (limit (sqrtApprox :: Integer -> r -> EnsureCN r)) x
+
+mysqrtNx ::
+  _ => Integer -> r -> r
+mysqrtNx n x =
+  foldl1 (.) (replicate n ((~!) . mysqrt)) x
+
+-- a rapid loss of accuracy, eg:
+_test_mysqrtNx :: MPBall
+_test_mysqrtNx =
+  mysqrtNx 10 (mpBallP (prec 10000) 2)
+-- [1.000677130693093 ± <2^(-18)]
+
+
+------------------------------------------------------
+-- sqrt example using limit WITH Lipschitz info
+------------------------------------------------------
+
+{-|
+  @mysqrt x@
+
+  preconditions: @x > 1@
+
+  returns $\sqrt{x}$.
+-}
+mysqrtL ::
+  _ => r -> EnsureCN r
+mysqrtL (x :: r) =
+  limit sqrtApproxL x
+  where
+  sqrtApproxL n =
+    WithLipschitz
+      (sqrtApprox n :: r -> EnsureCN r)
+      (\ _ -> (cn (half :: r)))
+  half = convertExactly (dyadic 0.5)
+
+mysqrtLNx ::
+  _ => Integer -> r -> r
+mysqrtLNx n x =
+  foldl1 (.) (replicate n ((~!) . mysqrtL)) x
+
+-- a much less rapid loss of accuracy, eg:
+_test_mysqrtLNx :: MPBall
+_test_mysqrtLNx =
+  mysqrtLNx 10 (mpBallP (prec 10000) 2)
+-- [1.000677130693066 ± <2^(-515)]
 
 ----------------
 -- redundant comparison
@@ -165,12 +205,15 @@ instance
 ---------
 
 class HasLimits ix s where
-  limit :: (ix -> s) -> s
+  type LimitType ix s
+  limit :: (ix -> s) -> LimitType ix s
 
 instance (HasLimits Accuracy t) => HasLimits Integer t where
+  type LimitType Integer t = LimitType Accuracy t
   limit s = limit (s . fromAccuracy)
 
 instance HasLimits Accuracy CauchyReal where
+  type LimitType Accuracy CauchyReal = CauchyReal
   limit s = newCR "limit" [] makeQ
     where
     makeQ (me, _src) ac@(AccuracySG acS _acG) =
@@ -179,6 +222,7 @@ instance HasLimits Accuracy CauchyReal where
       e = errorBound $ 0.5^!(fromAccuracy acS + 1)
 
 instance HasLimits Accuracy (CauchyReal -> CauchyRealCN) where
+  type LimitType Accuracy (CauchyReal -> CauchyRealCN) = (CauchyReal -> CauchyRealCN)
   limit fs x = newCRCN "limit" [AnyProtocolQA x] makeQ
     where
     makeQ (me, _src) ac@(AccuracySG acS _acG) =
@@ -194,7 +238,8 @@ instance HasLimits Accuracy (CauchyReal -> CauchyRealCN) where
   https://link.springer.com/chapter/10.1007/3-540-45335-0_14
 -}
 instance HasLimits Accuracy (MPBall -> CN MPBall) where
-  limit s x =
+  type LimitType Accuracy (MPBall -> CN MPBall) = (MPBall -> CN MPBall)
+  limit fs x =
     maybeTrace ("limit (MPBall -> CN MPBall): x = " ++ show x) $
     maybeTrace ("limit (MPBall -> CN MPBall): xPNext = " ++ show xPNext) $
     maybeTrace ("limit (MPBall -> CN MPBall): accuracies = " ++ show accuracies) $
@@ -206,7 +251,8 @@ instance HasLimits Accuracy (MPBall -> CN MPBall) where
       aux a
         | a >= 4 = bits a : aux (a `div` 2)
         | otherwise = [bits a]
-    xPNext = setPrecision (2 * (getPrecision x)) x
+    xPNext = setPrecision (increaseP $ getPrecision x) x
+    increaseP p = prec $ ((105 * (integer p)) `div` 100) + 1
 
     tryAccuracies [] =
       noValueNumErrorPotentialECN (Nothing :: Maybe MPBall) $
@@ -219,7 +265,57 @@ instance HasLimits Accuracy (MPBall -> CN MPBall) where
 
     withAccuracy ac =
       maybeTrace ("limit (MPBall -> CN MPBall): withAccuracy: ac = " ++ show ac) $
-      lift1CE (updateRadius (+ acEB)) (s ac xPNext)
+      lift1CE (updateRadius (+ acEB)) (fs ac xPNext)
       where
       acI = fromAccuracy ac
       acEB = errorBound $ 0.5^!acI
+
+data WithLipschitz f = WithLipschitz f f
+
+instance HasLimits Accuracy (WithLipschitz (MPBall -> CN MPBall)) where
+  type LimitType Accuracy (WithLipschitz (MPBall -> CN MPBall)) = (MPBall -> CN MPBall)
+  limit ffs xPre =
+    maybeTrace ("limit (MPBall -> CN MPBall): x = " ++ show x) $
+    maybeTrace ("limit (MPBall -> CN MPBall): xC = " ++ show xC) $
+    maybeTrace ("limit (MPBall -> CN MPBall): xE = " ++ show xE) $
+    maybeTrace ("limit (MPBall -> CN MPBall): accuracies = " ++ show accuracies) $
+    tryAccuracies accuracies
+    where
+    acX = getFiniteAccuracy x
+    accuracies = aux (fromAccuracy acX)
+      where
+      aux a
+        | a >= 4 = bits a : aux (a `div` 2)
+        | otherwise = [bits a]
+    x = increasePrec xPre
+      where
+      increasePrec z = setPrecision (inc $ getPrecision xPre) z
+      inc p = prec $ ((105 * (integer p)) `div` 100) + 1
+    xC = centreAsBall x
+    xE = radius x
+
+    tryAccuracies [] =
+      noValueNumErrorPotentialECN (Nothing :: Maybe MPBall) $
+        NumError "limit (MPBall -> CN MPBall) failed"
+    tryAccuracies (ac : rest) =
+      let result = withAccuracy ac in
+      case getErrorsCN result of
+        [] -> result
+        _ -> tryAccuracies rest
+
+    withAccuracy ac =
+      maybeTrace ("limit (MPBall -> CN MPBall): withAccuracy: ac = " ++ show ac) $
+      lift1CE (updateRadius (+ acEB)) fx
+      where
+      acI = fromAccuracy ac
+      acEB = errorBound $ 0.5^!acI
+      WithLipschitz f f' = ffs ac
+      fxC_CN = f xC
+      f'x_CN = f' x
+      fx :: CN MPBall
+      fx =
+          case (ensureNoCN fxC_CN, ensureNoCN f'x_CN) of
+            ((Just fxC, []), (Just f'x, [])) ->
+              cn $ updateRadius (+ (xE * (errorBound f'x))) fxC
+            _ ->
+              f x -- fallback
