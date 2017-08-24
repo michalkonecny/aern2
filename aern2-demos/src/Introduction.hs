572088648bd5141e78be874c8943a81f4eacde6c
mikkonecny <mikkonecny@gmail.com>
2017-08-24 13:10:36 +0100

    Introduction.hs: add more commentary and TODOs

--- aern2-demos/src/Introduction.hs
+++ aern2-demos/src/Introduction.hs
@@ -3,6 +3,7 @@ module Introduction where
 import MixedTypesNumPrelude
 
 import AERN2.Real
+import AERN2.Sequence
 
 -- import Debug.Trace
 
@@ -34,7 +35,7 @@ sumSines1 n = sum [sin i | i <- [1..n]]
 --    and guide: 120 bits
 sumSines1_run1 :: MPBall
 sumSines1_run1 = (sumSines1 100) ? (bitsSG 100 120)
-{- output:
+{- ghci log:
 *Introduction> sumSines1_run1
 [-0.127171013660420115436752171427 ± <2^(-112)]
 (0.02 secs, 0 bytes)
@@ -50,7 +51,7 @@ we miss the accuracy target.
 
 -- Request 100 bits of the result of the above expression with n = 100:
 sumSines1_run2 = (sumSines1 100) ? (bitsSG 100 100)
-{- output:
+{- ghci log:
   *Introduction> sumSines1_run2
   [-0.127171013660420115436752171426 ± <2^(-100)]
   (1.60 secs, 2,382,797,736 bytes)
@@ -69,31 +70,117 @@ sumSines1_run4 = print (sumSines1 100)
 -- real number comparisons
 ------------------------------
 
+{-
+  First consider comparisons of real number approximations.
+  These may be decided or undecided, using Maybe.
+-}
+
+pi100 :: MPBall
 pi100 = pi?(bitsS 100)
 
+compare_run1 :: Maybe Bool
 compare_run1 = pi100 > 0
-compare_run2 = pi100 == pi100
+-- returns: Just True
 
+compare_run2 = pi100 == pi100
+-- returns: Nothing
+
+{-
+  Comparison of real numbers are not decidable.
+  Nevertheless, the result of such a comparison
+  can be computably approximated using a sequence of Booleans.
+-}
+
+compare_run3 :: Sequence (Maybe Bool)
 compare_run3 = pi > 0
+-- in ghci prints: Just True
+-- (evaluated using default accuracy bitsS 100)
+
 compare_run4 = pi == pi + 0.5^!1000
-compare_run5 = (pi == pi + 0.5^!1000) ? (bitsS 100)
+-- in ghci prints: Nothing
+-- (evaluated using default accuracy bitsS 100)
+
+compare_run5 = (pi == pi + 0.5^!1000) ? (bitsS 1000)
+-- returns: Just False
+
 compare_run6 = real 0 == 0
+-- in ghci prints: Just True
+-- this is decided in finite time because 0 is represented exactly
+
+compare_run7 = pi == pi ? (bitsS 10000)
+-- returns: Nothing
+-- (cannot confirm pi=pi in finite time)
 
 ------------------------------
 -- checking partial functions
 ------------------------------
 
--- partialfn_run0 = sqrt (-1) :: R
-partialfn_run1 = sqrt (-1)
-partialfn_run2 = sqrt 0
-partialfn_run3 = sqrt (pi-pi)
-partialfn_run4 = (~!) (sqrt (pi-pi))
+{-
+  Normally in Haskell computation such as 1/0 or sqrt (-1) result
+  in a run-time exception.  When using AERN2 with mixed-types-num
+  (the default), these expressions do not throw exceptions, but
+  return special values that describe the error.  These values are
+  propagated through expressions and can be caught at any level of the expression.
+
+  Please see
+-}
+
+partialfn_run1 :: CN MPBall
+partialfn_run1 = sqrt (-1) ? (bitsS 100)
+{- ghci log:
+*Introduction> partialfn_run1
+{[(ERROR,out of range: sqrt: argument must be >= 0: [-1.0 ± 0])]}
+-}
+
+{- Now let us catch/inspect collected errors: -}
+partialfn_run2 :: Maybe MPBall
+partialfn_run2 =
+  let x = -1 in
+  case ensureNoCN (sqrt x ? (bitsS 100)) of
+    (Just r, _errors) -> Just r
+    (Nothing, _errors) -> Nothing
+
+partialfn_run3 = sqrt 0
+{- ghci log:
+*Introduction> partialfn_run3
+[0.0 ± 0]
+-}
+
+{-
+ When computing on approximations which do not have enough information
+ to check whether an error occurs, we get a *potential* error:
+-}
+
+partialfn_run4 = sqrt (pi-pi)
+{- ghci log:
+*Introduction> partialfn_run4
+[2.82118644197349001912496768933e-37 ± <2^(-121)]{[(POTENTIAL ERROR,out of range: sqrt: argument must be >= 0: [0.0 ± <2^(-240)])]}
+-}
+
+{-
+  When we are sure the potential error will not occur, we can erase it,
+  risking a run-time exception if we get it wrong:
+-}
+
+partialfn_run5 = (~!) (sqrt (pi-pi))
+{- ghci log:
+*Introduction> partialfn_run5
+[2.82118644197349001912496768933e-37 ± <2^(-121)]
+-}
+
+partialfn_run6 = (~!) (sqrt (pi-pi-1))
+{- ghci log:
+*Introduction> partialfn_run6
+*** Exception: WithGlobalParam ensureNoCE: []
+
+FIXME: the above should print a description of the exception...
+-}
 
 ---------------------------------
 -- "parallel" branching for real numbers
 --------------------------------
 
--- myabs0 :: R -> R
+myabs0 :: Rational -> Rational
 myabs0 x = if x < 0 then -x else x
 
 type RCN = CauchyRealCN
@@ -104,7 +191,9 @@ myabs1 x = if x < 0 then -x else x
 myabs2 :: R -> R
 myabs2 x = (~!) $ if x < 0 then -x else x
 
-{- lim -}
+---------------------------------
+-- Computing limits
+--------------------------------
 
 -- TODO
 
