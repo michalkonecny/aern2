0da96062834c2885f755fcd7bc79d82add43c7b2
mikkonecny <mikkonecny@gmail.com>
2017-08-24 11:49:52 +0100

    split ClosestPairDist from Demos, some commentary

--- aern2-demos/src/Demos.hs
+++ aern2-demos/src/Demos.hs
@@ -1,48 +1,73 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
-{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
-{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Demos where
 
 import MixedTypesNumPrelude
--- import qualified Prelude as P
--- import Text.Printf
 
-import Test.QuickCheck
-import qualified Data.List as List
-
-import AERN2.QA.Protocol
 import AERN2.Real
 
 -- import Debug.Trace
 
-{- type classes -}
+------------------------------
+-- real numbers
+------------------------------
 
--- toStrings :: (Show a) => [a] -> [String]
-toStrings xs = map show xs
-
--- toStrings_bad = toStrings [io_run1, io_run2]
-
-{- real numbers -}
-
+-- define a short name for the type of real numbers:
 type R = CauchyReal
--- Real numbers represented by fast converging Cauchy sequences of dyadic intervals
-type RA to = CauchyRealA to
--- Real numbers to which one can send an accuracy query in any QAArrow
+-- Real numbers are represented by fast converging sequences of dyadic intervals:
+--    CauchyReal = Sequence MPBall
+-- A sequence is essentially a function from accuracy, ie number of bits:
+--    Sequence t = ... (AccuracySG -> t)
 
--- internally:
--- CauchyReal = Sequence MPBall
--- Sequence t = ... (AccuracySG -> t)
-
-{- accuracy guide -}
-
+-- Start with a simple real number expression:
 sumSines1 n = sum [sin i | i <- [1..n]]
 
-sumSines1_run1 = sumSines1 100
-sumSines1_run2 = (sumSines1 100) ? (bitsSG 100 200)
-sumSines1_run3 = (sumSines1 100) ? (bitsSG 100 100)
+{-
+  You can run many of the following yourself in ghci.
+  If you installed AERN2 using the official instructions,
+  you can start ghci using the following command in the base
+  folder:
 
-{- comparisons -}
+  stack repl aern2/aern2-demos/src/Demos.hs
+-}
+
+-- Request accuracy
+--    strict limit: 100 bits
+--    and guide: 120 bits
+sumSines1_run1 :: MPBall
+sumSines1_run1 = (sumSines1 100) ? (bitsSG 100 120)
+{- output:
+*Demos> sumSines1_run1
+[-0.127171013660420115436752171427 ± <2^(-112)]
+(0.02 secs, 0 bytes)
+
+Note that the result accuracy is somewhat below
+the guide accuracy target of 120.  This is because the intermediate results are
+computed with an accuracy that is not entirely sufficient for the target accuracy
+of the final result.  When propagating requests to sub-expressions, we
+avoid raising the accuracy requests unnecessarily so that in deeply nested
+expressions the accuracy would not grow too fast. The price is that typically
+we miss the accuracy target.
+-}
+
+-- Request 100 bits of the result of the above expression with n = 100:
+sumSines1_run2 = (sumSines1 100) ? (bitsSG 100 100)
+{- output:
+  *Demos> sumSines1_run2
+  [-0.127171013660420115436752171426 ± <2^(-100)]
+  (1.60 secs, 2,382,797,736 bytes)
+
+This is very slow because there is a lot of backtracking when target accuracy
+is not reached.  Thus one should avoid making strict limit and guide too close.
+
+The following query will automatically set the guide accuracy 20 bits above the strict limit:
+-}
+sumSines1_run3 = (sumSines1 100) ? (bitsS 100)
+-- which is the default request when printing a CauchyReal:
+sumSines1_run4 = print (sumSines1 100)
+
+
+------------------------------
+-- real number comparisons
+------------------------------
 
 pi100 = pi?(bitsS 100)
 
@@ -130,94 +155,6 @@ irramEval f x =
       where
       xWithP = (setPrecision p (x ? (accuracySG $ bits p)))
 
-
-----------------------------------
--- Finding the smallest distance within a set of real numbers
-----------------------------------
-
-closestPairDist_naive ::
-  _ => [t] -> t
-closestPairDist_naive pts
-  | length pts < 2 = error "closestPairDist_naive: too few points"
-  | otherwise =
-      (foldl1 min (map distance (distinctPairs pts)))
-
-distance :: (CanSubSameType t, CanAbsSameType t) => (t, t) -> t
-distance (a,b) = abs (a-b)
-
-closestPairDist_run ::
-  _ =>
-  ([t] -> t) ->
-  Integer -> t
-closestPairDist_run (closestPairDist :: [t] -> t) n =
-  closestPairDist [sin (convertExactly i :: t) | i <- [1..n]]
-
-{- specification and randomised tests -}
-
-closestPairDist_spec closestPairDist (getFinite :: r -> t) numbers =
-  (length numbers) < 2
-  .||.
-  (getFinite (closestPairDist numbersR)) ?==?$ (closestPairDist_naive numbers)
-  where
-  numbersR = map convertExactly numbers :: [r]
-  a ?==?$ b = printArgsIfFails2 "?==?" (?==?) a b
-
-closestPairDist_runTests1 =
-  quickCheck (closestPairDist_spec (closestPairDist_split compRApprox) (?acStd) :: [Integer] -> Property)
-closestPairDist_runTests2 =
-  quickCheck (closestPairDist_spec (closestPairDist_split compMPBall) id :: [Integer] -> Property)
-
-acStd = bitsSG 100 120
-
-sample_integers = sample' (arbitrary :: Gen [Integer]) >>= mapM_ print
-sample_rationals = sample' (arbitrary :: Gen [Rational]) >>= mapM_ print
-
-{- a version that splits, recurses and merges the results -}
-closestPairDist_split ::
-  _ => (t -> t -> Bool) -> [t] -> t
-closestPairDist_split (.<) pts
-  | length ptsL < 2 || length ptsR < 2 =
-      closestPairDist_naive pts
-  | otherwise =
-      recurseAndMerge
-  where
-  (ptsL,ptsR) = List.partition isCertainlyLeft pts
-    where
-    isCertainlyLeft x = x .< average pts
-  recurseAndMerge =
-    -- foldl1 min [dL, dLR, dR]
-    foldl1 min [dL, dR]
-    where
-    dL = closestPairDist_split (.<) ptsL
-    -- dLR = distance (largest ptsL, smallest ptsR)
-    dR = closestPairDist_split (.<) ptsR
-
-compRApprox :: R -> R -> Bool
-compRApprox a b = (a?ac) !<! (b?ac)
-  where
-  ac = bitsS 100
-
-compMPBall :: MPBall -> MPBall -> Bool
-compMPBall = (!<!)
-
-{- auxiliary functions -}
-
-hull :: MPBall -> MPBall -> MPBall
-hull = fromEndpoints
-
-average :: (HasIntegers t, CanAddSameType t, CanDivCNBy t Integer) => [t] -> t
-average xs = (sum xs) /! (length xs)
-
-largest :: (CanMinMaxSameType t) => [t] -> t
-largest pts = foldl1 max pts
-
-smallest :: (CanMinMaxSameType t) => [t] -> t
-smallest pts = foldl1 min pts
-
-distinctPairs :: [t] -> [(t,t)]
-distinctPairs xs = [(x,y) | (x:rest) <- tails1 xs, y <- rest]
-
-{-| non-empty tails -}
-tails1 :: [t] -> [[t]]
-tails1 list =
-  take (length list - 1) $ List.tails list
+{-
+  Recommended further reading:  ClosestPairDist.hs
+-}
