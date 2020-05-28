4cc94e1fb95594146d09c3710947eefddc4e70f2
Junaid Rasheed <jrasheed178@gmail.com>
2020-05-28 12:14:11 +0100

    Refactor BoxFunMinMax Optimisation functions
    + Functions return Maybe Bool instead of a number
      + Because we want to know if the minimum of a function is above n
    + Remove string from MinMaxTree leaves
      + Was used for debugging
    + Create globalMinimumAbove0Tree function
      + Calls globalMinimum on a given tree
      + No splitting at the moment

--- aern2-mfun/src/AERN2/BoxFunMinMax/Optimisation.hs
+++ aern2-mfun/src/AERN2/BoxFunMinMax/Optimisation.hs
@@ -10,13 +10,6 @@ import AERN2.Linear.Vector.Type as V
 import Data.Maybe
 import qualified Data.List as List
 
-import AERN2.Linear.Vector.Type (Vector, (!))
-import AERN2.Linear.Vector.Type as V
-import AERN2.Linear.Matrix.Type
-import AERN2.Linear.Matrix.Inverse
-
-import qualified Data.List as List
-
 import qualified AERN2.PQueue as Q
 
 
@@ -28,33 +21,22 @@ globalMinimumGreaterThanN :: BoxFun -> Accuracy -> Precision -> CN Rational -> M
 globalMinimumGreaterThanN f ac initialPrecision n =
     case globalMin of
         Just x  -> 
-            trace ("Global minimum found, minimum = " ++ show x ++ "for f at domain " ++ show (getEndpoints (domain f)))
-            Just (x !>! n)
+            trace ("Global minimum found, minimum = " ++ show x ++ " for f at domain " ++ show (getEndpoints (domain f)))
+            Just x
         Nothing -> Nothing
     where globalMin = globalMinimumAboveN f ac initialPrecision (cnMPBallP initialPrecision n)
 
----
-
-globalMinimumWithCutoffAboveN :: BoxFun -> Accuracy -> CN MPBall -> Precision -> CN MPBall -> Maybe (CN MPBall)
+globalMinimumWithCutoffAboveN :: BoxFun -> Accuracy -> CN MPBall -> Precision -> CN MPBall -> Maybe Bool
 globalMinimumWithCutoffAboveN f ac cutoff initialPrecision n =
     if dimension f == 1 then
         let
             fl         = apply f (V.map lowerBound $ domain f)
             fr         = apply f (V.map upperBound $ domain f)
             mLocalMin   = bestLocalMinimumWithCutoffAboveN f (domain f) ac cutoff initialPrecision n
-            currentMin = case mLocalMin of
-                            Just localMin  -> min fl $ min (snd localMin) fr
-                            Nothing -> min fl fr
         in
             case mLocalMin of
-                Just _ ->
-                    if currentMin !>! n then
-                        Just currentMin
-                    else
-                        Nothing
-                Nothing ->
-                    Nothing
-                
+                Just (False, _) -> Just False
+                _               -> Just (fl !>! n && fr !>! n)
     else 
         let
             mLocalMin       = bestLocalMinimumWithCutoffAboveN f (domain f) ac cutoff initialPrecision n
@@ -62,19 +44,26 @@ globalMinimumWithCutoffAboveN f ac cutoff initialPrecision n =
         in
             case mLocalMin of
                 Just localMin ->
-                    Just (List.foldl' min (snd localMin) boundaryMinima)
-                    where
-                        boundaryMinima = mapMaybe (\g -> globalMinimumWithCutoffAboveN g ac (min cutoff ((upperBound (snd localMin) :: CN MPBall))) initialPrecision n) boundaryFuns
+                    (&&) <$> Just (fst localMin) <*> boundaryMinima boundaryFuns (min cutoff ((upperBound (snd localMin) :: CN MPBall)))
                 Nothing ->
-                    Nothing
+                    boundaryMinima boundaryFuns cutoff
+                where
+                    boundaryMinima  []      _       =   Nothing
+                    boundaryMinima  [g]     cutoff  =   case globalMinimumWithCutoffAboveN g ac cutoff initialPrecision n of
+                                                            Just False  -> Just False
+                                                            _           -> Just True
+                    boundaryMinima  (g:gs)  cutoff  =   case globalMinimumWithCutoffAboveN g ac cutoff initialPrecision n of
+                                                            Just False  -> Just False
+                                                            _           -> boundaryMinima gs cutoff
 
-globalMinimumAboveN :: BoxFun -> Accuracy -> Precision -> CN MPBall -> Maybe (CN MPBall)
+
+globalMinimumAboveN :: BoxFun -> Accuracy -> Precision -> CN MPBall -> Maybe Bool
 globalMinimumAboveN f ac initialPrecision n =
     globalMinimumWithCutoffAboveN f ac (apply f (centre boxp)) initialPrecision n
     where
     boxp = setPrecision initialPrecision (domain f)
 
-bestLocalMinimumWithCutoffAboveN :: BoxFun -> Box -> Accuracy -> CN MPBall -> Precision -> CN MPBall -> Maybe (Integer, CN MPBall)
+bestLocalMinimumWithCutoffAboveN :: BoxFun -> Box -> Accuracy -> CN MPBall -> Precision -> CN MPBall -> Maybe (Bool, CN MPBall)
 bestLocalMinimumWithCutoffAboveN f box ac initialCutoff initialPrecision n =
     aux initialQueue initialCutoff 0 dummyBox
     where
@@ -101,12 +90,13 @@ bestLocalMinimumWithCutoffAboveN f box ac initialCutoff initialPrecision n =
                 --trace ("dist to last "++ (show $ distToLast)) $
                 --trace ("accuracy: "++ (show $ getAccuracy val')) $
                 --trace ("precision centre: "++ (show $ fmap (getPrecision . centre) val)) $
-                if val' !<! n then
+                if cutoff !<! n then
                     trace ("f is under " ++ show n ++ " in domain: " ++ show (AERN2.BoxFun.Box.getEndpoints ext))
-                    Nothing
+                    Just (False, cutoff)
                 else
                     if getAccuracy val' >= ac then
-                        Just (steps, val')
+                        -- TODO: if val' >=n then Just True
+                        Just (True, val')
                     else 
                         aux q'' newCutoff (steps + 1) (SearchBox ext rng)
                 where
--- aern2-mfun/src/AERN2/BoxFunMinMax/Type.hs
+++ aern2-mfun/src/AERN2/BoxFunMinMax/Type.hs
@@ -7,73 +7,79 @@ import AERN2.BoxFun.Box
 import AERN2.BoxFun.TestFunctions
 import AERN2.BoxFunMinMax.Optimisation
 
+import Data.Maybe
+
 import Debug.Trace
 
 -- All leaves in the tree must have the same domain
-data MinMaxTree = Leaf {tree_f :: (BoxFun, String)} | Min {tree_l :: MinMaxTree, tree_r :: MinMaxTree} | Max {tree_l :: MinMaxTree, tree_r :: MinMaxTree}
+data MinMaxTree = Leaf {tree_f :: BoxFun} | Min {tree_l :: MinMaxTree, tree_r :: MinMaxTree} | Max {tree_l :: MinMaxTree, tree_r :: MinMaxTree}
 
-heronTree :: MinMaxTree
-heronTree = Max 
+heronTree2 :: MinMaxTree
+heronTree2 = Max 
             (Min heron1 heron2)
             (Min heron3 heron4)
             
 heron1 :: MinMaxTree
-heron1 = Max (Leaf (heron1p, "heron1p")) (Leaf (heron1q,"heron1q"))
+heron1 = Max (Leaf (heron1p)) (Leaf (heron1q))
 
 heron2 :: MinMaxTree
-heron2 = Max (Leaf (heron2p, "heron2p")) (Leaf (heron2q, "heron2q"))
+heron2 = Max (Leaf (heron2p)) (Leaf (heron2q))
 
 heron3 :: MinMaxTree
-heron3 = Max (Leaf (heron3p, "heron3p")) (Leaf (heron3q, "heron3q"))
+heron3 = Max (Leaf (heron3p)) (Leaf (heron3q))
 
 heron4 :: MinMaxTree
-heron4 = Max (Leaf (heron4p, "heron4p")) (Leaf (heron4q, "heron4p"))
+heron4 = Max (Leaf (heron4p)) (Leaf (heron4q))
 
-checkTree :: MinMaxTree -> Box -> Accuracy -> Precision -> Bool
+checkTree :: MinMaxTree -> Box -> Accuracy -> Precision -> Maybe Bool
 checkTree (Leaf f) box ac p = 
-  trace ("Checking " ++ snd f ++ "on domain: " ++ show (getEndpoints box))
-  trace ("rough bound gives: " ++ show ((applyMinimumOnBox (fst f) box')))
-  (applyMinimumOnBox (fst f) box' !>! 0)
+  trace ("Checking on domain: " ++ show (getEndpoints box))
+  trace ("rough bound gives: " ++ show ((applyMinimumOnBox (f) box')))
+  Just (applyMinimumOnBox (f) box' !>! 0)
   ||
-  case globalMinimumGreaterThanN (BoxFun (dimension (fst f)) (bf_eval (fst f)) box') ac p (cn 0.0) of
-    Just b  -> b
-    Nothing -> False
+  globalMinimumGreaterThanN (BoxFun (dimension (f)) (bf_eval (f)) box') ac p (cn 0.0)
   where
     box' = setPrecision p box
 checkTree (Max l r) box ac p
-  | lRange !>! rRange = 
-    trace "Left branch greater than right branch"
+  | rRange !<! 0 =
+    trace "Right branch below 0"
     trace ("Left  range: " ++ show (endpointsAsIntervals lRange))
     trace ("Right range: " ++ show (endpointsAsIntervals rRange))
     checkTree l box ac p
 
-  | rRange !>! lRange = 
-    trace "Right branch greater than left branch"
+  | lRange !<! 0 =
+    trace "Left branch below 0"
     trace ("Left  range: " ++ show (endpointsAsIntervals lRange))
     trace ("Right range: " ++ show (endpointsAsIntervals rRange))
     checkTree r box ac p
-
   | otherwise      =
-    (lRange !>! 0 || rRange !>! 0)
+    (lRange !>! 0 || rRange !>! 0) -- || (lOptAboveZero || rOptAboveZero)
     ||
-    (if AERN2.BoxFun.Box.width box' !>! 1 / (fromAccuracy ac ^ 2) then
+    (if AERN2.BoxFun.Box.width box' !>! 1 / (200) then
         trace ("Bisected boxes: " ++ show newBoxes)
         checkBoxes newBoxes
       else
-        trace "Stopping bisections (Box too small)"
-        checkTree l box' ac p || checkTree r box' ac p)
+        trace "Stopping bisections (Box too small)" $
+        case (checkTree l box' ac p, checkTree r box' ac p) of
+          (Just True, _) -> Just True
+          (_, Just True) -> Just True
+          (_, _)         -> trace "indeterminate" 
+                            Nothing)
     where
       lRange = applyTree l box'
       rRange = applyTree r box'
+      
+      lOptAboveZero = fromMaybe False (globalMinimumAbove0Tree l box')
+      rOptAboveZero = fromMaybe False (globalMinimumAbove0Tree r box')
  
       box' = setPrecision p box
 
       newBoxes = fullBisect box'
 
-      checkBoxes []           = True
+      checkBoxes []           = Just True
       checkBoxes [b]          = checkTree (Max l r) b ac p
-      checkBoxes (b : boxes)  = checkBoxes [b]
-                                &&
+      checkBoxes (b : boxes)  = (&&) <$> checkBoxes [b]
+                                <*>
                                 checkBoxes boxes
 checkTree (Min l r) box ac p = checkTree l box ac p && checkTree r box ac p
 
@@ -83,11 +89,20 @@ size (Min l r) = 1 + size l + size r
 size (Max l r) = 1 + size l + size r
 
 applyTree :: MinMaxTree -> Box -> CN MPBall
-applyTree (Leaf f)  box = apply (fst f) box
+
+-- applyTree (Leaf f)  box = globalMinimumAboveN (BoxFun (dimension (fst f)) (bf_eval (fst f)) box) (bits 100) (prec 1000) (cn 0.0)
+applyTree (Leaf f)  box = apply (f) box
 applyTree (Max l r) box = max (applyTree l box) (applyTree r box)
 applyTree (Min l r) box = min (applyTree l box) (applyTree r box)
 
+-- TODO: globalMinimum and globalMaximum with low accuracy/precision to compute more accurate r and lRanges
+
 domainTree :: MinMaxTree -> Box
-domainTree (Leaf f) = domain (fst f) 
+domainTree (Leaf f) = domain (f) 
 domainTree (Min l _) = domainTree l 
 domainTree (Max l _) = domainTree l
+
+globalMinimumAbove0Tree :: MinMaxTree -> Box -> Maybe Bool
+globalMinimumAbove0Tree (Leaf f) box  = globalMinimumGreaterThanN (BoxFun (dimension (f)) (bf_eval (f)) box) (bits 10) (prec 10) (cn 0.0)
+globalMinimumAbove0Tree (Max l r) box = (||) <$> globalMinimumAbove0Tree l box <*> globalMinimumAbove0Tree r box
+globalMinimumAbove0Tree (Min l r) box = (&&) <$> globalMinimumAbove0Tree l box <*> globalMinimumAbove0Tree r box
