6c40e3a1cdcff4cddda3d62586e3f5495a795b83
Junaid Rasheed <jrasheed178@gmail.com>
2020-06-16 21:24:42 +0100

    Parallelise checkTree, support for counterexamples
    + BoxFunMinMax.Optimisation functions will specify indeterminate results
      + And return counterexample for False/Indeterminate results
    + Parallelise checkTree using Control.Parallel
      + Pro: can be nested. Let compiler deal with creating threads
      + Con: all arguments in a 'tuple' must be evaluated, sometimes results in unneeded work

--- aern2-mfun/src/AERN2/BoxFunMinMax/Optimisation.hs
+++ aern2-mfun/src/AERN2/BoxFunMinMax/Optimisation.hs
@@ -2,7 +2,7 @@ module AERN2.BoxFunMinMax.Optimisation where
 
 import MixedTypesNumPrelude
 import AERN2.MP.Ball
-import AERN2.BoxFun.Box (Box, getEndpoints)
+import AERN2.BoxFun.Box (Box, getEndpoints, width)
 import AERN2.BoxFun.Optimisation
 import AERN2.BoxFun.Type
 import AERN2.Linear.Vector.Type as V
@@ -17,55 +17,41 @@ import AERN2.Util.Util
 
 import Debug.Trace (trace)
 
-globalMinimumGreaterThanN :: BoxFun -> Accuracy -> Precision -> CN Rational -> Maybe Bool
-globalMinimumGreaterThanN f ac initialPrecision n =
-    case globalMin of
-        Just x  -> 
-            trace ("Global minimum found, minimum = " ++ show x ++ " for f at domain " ++ show (getEndpoints (domain f)))
-            Just x
-        Nothing -> Nothing
-    where globalMin = globalMinimumAboveN f ac initialPrecision (cnMPBallP initialPrecision n)
+globalMinimumGreaterThanN :: BoxFun -> Accuracy -> Precision -> CN MPBall -> CN Rational -> (Maybe Bool, Maybe SearchBox)
+globalMinimumGreaterThanN f ac initialPrecision widthCutoff n =
+    globalMinimumAboveN f ac initialPrecision widthCutoff (cnMPBallP initialPrecision n)
 
-globalMinimumWithCutoffAboveN :: BoxFun -> Accuracy -> CN MPBall -> Precision -> CN MPBall -> Maybe Bool
-globalMinimumWithCutoffAboveN f ac cutoff initialPrecision n =
+globalMinimumAboveN :: BoxFun -> Accuracy -> Precision -> CN MPBall -> CN MPBall -> (Maybe Bool, Maybe SearchBox)
+globalMinimumAboveN f ac initialPrecision widthCutoff n =
     if dimension f == 1 then
         let
             fl         = apply f (V.map lowerBound $ domain f)
             fr         = apply f (V.map upperBound $ domain f)
-            mLocalMin   = bestLocalMinimumWithCutoffAboveN f (domain f) ac cutoff initialPrecision n
         in
-            case mLocalMin of
-                Just (False, _) -> Just False
-                _               -> Just (fl !>! n && fr !>! n)
+            if fl !<=! n then
+                (Just False, Just (SearchBox (V.map lowerBound (domain f)) fl))
+            else
+                if fr !<=! n then
+                    (Just False, Just (SearchBox (V.map upperBound (domain f)) fr))
+                else
+                    minimumAboveN f (domain f) ac initialPrecision widthCutoff n
     else 
         let
-            mLocalMin       = bestLocalMinimumWithCutoffAboveN f (domain f) ac cutoff initialPrecision n
-            boundaryFuns   = boundaryRestrictions f
+            boundaryFuns = boundaryRestrictions f
         in
-            case mLocalMin of
-                Just localMin ->
-                    (&&) <$> Just (fst localMin) <*> boundaryMinima boundaryFuns (min cutoff ((upperBound (snd localMin) :: CN MPBall)))
-                Nothing ->
-                    boundaryMinima boundaryFuns cutoff
+                    case minimumAboveN f (domain f) ac initialPrecision widthCutoff n of
+                        (Just True, _)  -> boundaryMinima boundaryFuns
+                        o               -> o 
                 where
-                    boundaryMinima  []      _       =   Nothing
-                    boundaryMinima  [g]     cutoff  =   case globalMinimumWithCutoffAboveN g ac cutoff initialPrecision n of
-                                                            Just False  -> Just False
-                                                            _           -> Just True
-                    boundaryMinima  (g:gs)  cutoff  =   case globalMinimumWithCutoffAboveN g ac cutoff initialPrecision n of
-                                                            Just False  -> Just False
-                                                            _           -> boundaryMinima gs cutoff
+                    boundaryMinima  []      =   (Just True, Nothing)
+                    boundaryMinima  (g:gs)  =  
+                        case globalMinimumAboveN g ac initialPrecision widthCutoff n of
+                            (Just True, _)  -> boundaryMinima gs
+                            o               -> o
 
-
-globalMinimumAboveN :: BoxFun -> Accuracy -> Precision -> CN MPBall -> Maybe Bool
-globalMinimumAboveN f ac initialPrecision n =
-    globalMinimumWithCutoffAboveN f ac (apply f (centre boxp)) initialPrecision n
-    where
-    boxp = setPrecision initialPrecision (domain f)
-
-bestLocalMinimumWithCutoffAboveN :: BoxFun -> Box -> Accuracy -> CN MPBall -> Precision -> CN MPBall -> Maybe (Bool, CN MPBall)
-bestLocalMinimumWithCutoffAboveN f box ac initialCutoff initialPrecision n =
-    aux initialQueue initialCutoff 0 dummyBox
+minimumAboveN :: BoxFun -> Box -> Accuracy -> Precision -> CN MPBall -> CN MPBall -> (Maybe Bool, Maybe SearchBox) -- TODO: Maybe Bool
+minimumAboveN f box ac initialPrecision widthCutoff n =
+    aux initialQueue 0 dummyBox
     where
     boxp             = setPrecision initialPrecision box
     initialRange     = apply f boxp
@@ -73,37 +59,57 @@ bestLocalMinimumWithCutoffAboveN f box ac initialCutoff initialPrecision n =
     initialQueue     = Q.singleton initialSearchBox
     dummyBox         = SearchBox (V.fromList [cn $ mpBall $ 10^!6]) initialRange -- TODO: hack...
 
-    aux q cutoff steps (SearchBox _lastBox rng) =  
+    aux q steps (SearchBox _lastBox rng) =  
         case Q.minView q of
-            Nothing -> trace ("no local minimum.") $ Nothing
+            Nothing -> 
+                -- If this is exhaustive
+                -- we know that the upper bound of f is not under n
+                trace ("f is above " ++ show n) $ (Just True, Nothing)
             Just (minBox, q') ->
-                --trace ("value: "++ (show $ val)) $
+                trace ("value: "++ (show $ val)) $
                 trace ("min box: "++ (show $ minBox)) $
                 --trace ("box acc: "++ (show $ getAccuracy $ ext)) $
                 --trace (show $ Box.width (extents minBox)) $
                 --trace ("lower bound "++ (show $ Box.lowerBound $ val)) $
                 --trace ("val' "++ (show $ val')) $
-                trace ("cutoff: "++ (show $ cutoff)) $
+                -- trace ("cutoff: "++ (show $ cutoff)) $
                 trace ("queue size: "++ (show $ Q.size q)) $
                 --trace ("cutoff == 0? "++(show $ cutoff == (mpBall 0))) $
                 --trace ("precision: "++ (show $ precision)) $
                 --trace ("dist to last "++ (show $ distToLast)) $
                 --trace ("accuracy: "++ (show $ getAccuracy val')) $
                 --trace ("precision centre: "++ (show $ fmap (getPrecision . centre) val)) $
-                if cutoff !<! n then
-                    trace ("f is under " ++ show n ++ " in domain: " ++ show (AERN2.BoxFun.Box.getEndpoints ext))
-                    Just (False, cutoff)
+                if upperBound val !<=! n then
+                    trace ("f is under " ++ show n ++ " in domain: " ++ show (AERN2.BoxFun.Box.getEndpoints ext) ++ " with val: " ++ show (upperBound val))
+                    (Just False, Just minBox)
                 else
-                    if getAccuracy val' >= ac then
-                        -- TODO: if val' >=n then Just True
-                        Just (True, val')
-                    else 
-                        aux q'' newCutoff (steps + 1) (SearchBox ext rng)
+                    if (lowerBound val !>! n) then
+                        trace ("f is above " ++ show n ++ " in domain: " ++ show (AERN2.BoxFun.Box.getEndpoints ext) ++ " with lower bound of val: " ++ show (lowerBound val))
+                        aux q' (steps + 1) (SearchBox ext rng)
+                    else
+                        if (width (extents minBox) !>=! widthCutoff) then -- TODO: mpBallP parameter
+                            trace "processing boxes"
+                            trace (show (width (extents minBox)))
+                            trace (show newBoxes)
+                            aux q'' (steps + 1) (SearchBox ext rng)
+                        else
+                            (Nothing, Just minBox)
+                        -- Some way to terminate the algorithm and return Nothing. Maybe check the size of the box
                 where
-                val' = fromEndpointsAsIntervals (lowerBound val) (cutoff)
                 SearchBox ext val = minBox
 
-                (newCutoff, newBoxes) = 
-                    processBox f ac cutoff minBox
+                -- cutoff is upper bound of the range of f on the entire domain? 
+
+
+                -- TODO:
+                -- refactor this
+                -- If we find a minimum below N, we stop
+                -- when we get to the nothing case, we can return True (because we've checked all possible minimums)
+
+                -- newton step shrinks the box with the guarantee that the part where the minimum is will not go away
+                -- focuses on parts where the derivative is zero
+
+                (_, newBoxes) = 
+                    processBox f ac (apply f (centre box)) minBox
 
                 q'' = foldr (Q.insert) q' newBoxes
--- aern2-mfun/src/AERN2/BoxFunMinMax/Type.hs
+++ aern2-mfun/src/AERN2/BoxFunMinMax/Type.hs
@@ -4,6 +4,7 @@ import AERN2.MP.Ball
 import MixedTypesNumPrelude
 import AERN2.BoxFun.Type
 import AERN2.BoxFun.Box
+import AERN2.BoxFun.Optimisation (SearchBox)
 import AERN2.BoxFun.TestFunctions
 import AERN2.BoxFunMinMax.Optimisation
 
@@ -11,8 +12,118 @@ import Data.Maybe
 
 import Debug.Trace
 
+import Control.Concurrent
+import Control.Concurrent.STM
+import System.IO.Unsafe
+
+import Control.Parallel
+import Control.Parallel.Strategies
+
+parallelOr :: (Maybe Bool, Maybe SearchBox) -> (Maybe Bool, Maybe SearchBox) -> (Maybe Bool, Maybe SearchBox)
+parallelOr b1 b2 =
+    result
+  where
+    tuple = (b1, b2) `using` (parTuple2 (dot rpar rseq) (dot rpar rseq))
+    -- b1' = b1 `using` r0
+    -- b2' = b2 `using` r0
+    result = case (tuple) of
+      (r@(Just True, _), _) -> r
+      (_, r@(Just True, _)) -> r
+      (r@(Nothing, _), _)   -> r
+      (_, r@(Nothing, _))   -> r
+      (r, _)                -> r
+      -- ((o1, b1), (o2, b2)) -> (o1, b1)
+
+parallelAnd :: (Maybe Bool, Maybe SearchBox) -> (Maybe Bool, Maybe SearchBox) -> (Maybe Bool, Maybe SearchBox)
+parallelAnd b1 b2 =
+  result
+  where
+    tuple = (b1, b2) `using` (parTuple2 (dot rpar rseq) (dot rpar rseq))
+    -- b1' = b1 `using` r0
+    -- b2' = b2 `using` r0
+    result = case (tuple) of
+      (r@(Just True, _), (Just True, _)) -> r
+      (r@(Nothing, _), _)                -> r
+      (_, r@(Nothing, _))                -> r
+      (r@(Just False, _), _)             -> r
+      (_, r@(Just False, _))             -> r
+      -- ((o1, b1), (o2, b2)) -> (o1, b1)
+
+por :: (Maybe Bool, Maybe SearchBox) -> (Maybe Bool, Maybe SearchBox) -> (Maybe Bool, Maybe SearchBox)
+por b1 b2 =
+  unsafePerformIO $ do
+    print "creating threads"
+    result1TV <- atomically $ newTVar Nothing
+    result2TV <- atomically $ newTVar Nothing
+    thread1 <- forkIO (atomically (seq b1 $ writeTVar result1TV (Just b1)))
+    thread2 <- forkIO (atomically (seq b2 $ writeTVar result2TV (Just b2)))
+    result <- atomically $ do
+      maybeResult1 <- readTVar result1TV
+      maybeResult2 <- readTVar result2TV
+      case (maybeResult1, maybeResult2) of
+        (Just r@(Just True, _), _)                     -> pure r
+        (_, Just r@(Just True, _))                     -> pure r
+        (Just r@(Nothing, _), Just _)                  -> pure r
+        (Just _, Just r@(Nothing, _))                  -> pure r
+        (Just r@(Just False, _), Just (Just False, _)) -> pure r
+        _ -> retry
+    print "killing thread 1 and thread 2"
+    killThread thread1
+    killThread thread2
+    pure result
+pand :: (Maybe Bool, Maybe SearchBox) -> (Maybe Bool, Maybe SearchBox) -> (Maybe Bool, Maybe SearchBox)
+pand b1 b2 =
+  unsafePerformIO $ do
+    result1TV <- atomically $ newTVar Nothing
+    result2TV <- atomically $ newTVar Nothing
+    thread1 <- forkIO (atomically (seq b1 $ writeTVar result1TV (Just b1)))
+    thread2 <- forkIO (atomically (seq b2 $ writeTVar result2TV (Just b2)))
+    result <- atomically $ do
+      maybeResult1 <- readTVar result1TV
+      maybeResult2 <- readTVar result2TV
+      case (maybeResult1, maybeResult2) of
+        (Just r@(Just True, _), Just (Just True, _))   -> pure r
+        (Just r@(Nothing, _), _)                       -> pure r
+        (_, Just r@(Nothing, _))                       -> pure r
+        (Just r@(Just False, _), _)                    -> pure r
+        (_, Just r@(Just False, _))                    -> pure r
+        _ -> retry
+    killThread thread1
+    killThread thread2
+    pure result
+
+-- pand1 = pand True True
+-- pand2 = pand True False
+-- pand3 = pand False True
+-- pand4 = pand False False
+-- pand5 = pand loop False
+-- pand6 = pand False loop
+
+loop = loop
+
+por1 = por (Just True, Nothing) (Just False, Nothing)
+por2 = por (Just False, Nothing) (Just True, Nothing)
+por3 = por (Just True, Nothing) (Just True, Nothing)
+por4 = por (Just True, Nothing) (Nothing, Nothing)
+por5 = por (Nothing, Nothing) (Just True, Nothing)
+por6 = por (Nothing, Nothing) (Nothing, Nothing)
+por7 = por (Just False, Nothing) (Just False, Nothing)
+por8 = por (Just True, Nothing) loop
+por9 = por loop (Just True, Nothing)
+
+
+pand1 = pand (Just True, Nothing) (Just False, Nothing)
+pand2 = pand (Just False, Nothing) (Just True, Nothing)
+pand3 = pand (Just True, Nothing) (Just True, Nothing)
+pand4 = pand (Nothing, Nothing) (Just True, Nothing)
+pand5 = pand (Just True, Nothing) (Nothing, Nothing)
+pand6 = pand (Nothing, Nothing) (Nothing, Nothing)
+pand7 = pand (Just False, Nothing) (Just False, Nothing)
+
 -- All leaves in the tree must have the same domain
+-- leaves should be bf_eval
 data MinMaxTree = Leaf {tree_f :: BoxFun} | Min {tree_l :: MinMaxTree, tree_r :: MinMaxTree} | Max {tree_l :: MinMaxTree, tree_r :: MinMaxTree}
+-- TODO: Refactor tree to take Min/Max of a list of trees
 
 heronTree2 :: MinMaxTree
 heronTree2 = Max 
@@ -31,57 +142,69 @@ heron3 = Max (Leaf (heron3p)) (Leaf (heron3q))
 heron4 :: MinMaxTree
 heron4 = Max (Leaf (heron4p)) (Leaf (heron4q))
 
-checkTree :: MinMaxTree -> Box -> Accuracy -> Precision -> Maybe Bool
-checkTree (Leaf f) box ac p = 
-  trace ("Checking on domain: " ++ show (getEndpoints box))
-  trace ("rough bound gives: " ++ show ((applyMinimumOnBox (f) box')))
-  Just (applyMinimumOnBox (f) box' !>! 0)
-  ||
-  globalMinimumGreaterThanN (BoxFun (dimension (f)) (bf_eval (f)) box') ac p (cn 0.0)
+checkTree :: MinMaxTree -> Box -> Accuracy -> Precision -> Rational -> (Maybe Bool, Maybe SearchBox)
+checkTree (Leaf f) box ac p n = 
+  trace ("Checking on domain: " ++ show (getEndpoints box)) $
+  trace ("rough bound gives: " ++ show ((applyMinimumOnBox f box'))) $
+  trace (show (unsafePerformIO getNumCapabilities)) $
+  if applyMinimumOnBox f box' !>! n then
+    (Just True, Nothing)
+  else
+    globalMinimumGreaterThanN f' ac p ((width box') / 10000000) (cn n)
   where
+    f' = BoxFun (dimension f) (bf_eval f) box'
     box' = setPrecision p box
-checkTree (Max l r) box ac p
-  | rRange !<! 0 =
-    trace "Right branch below 0"
+checkTree (Max l r) box ac p n
+  | rRange !<! n =
+    trace ("Right branch below " ++ show n)
     trace ("Left  range: " ++ show (endpointsAsIntervals lRange))
     trace ("Right range: " ++ show (endpointsAsIntervals rRange))
-    checkTree l box ac p
+    checkTree l box ac p n
 
-  | lRange !<! 0 =
-    trace "Left branch below 0"
+  | lRange !<! n =
+    trace ("Left branch below " ++ show n)
     trace ("Left  range: " ++ show (endpointsAsIntervals lRange))
     trace ("Right range: " ++ show (endpointsAsIntervals rRange))
-    checkTree r box ac p
+    checkTree r box ac p n
   | otherwise      =
-    (lRange !>! 0 || rRange !>! 0) -- || (lOptAboveZero || rOptAboveZero)
-    ||
-    (if AERN2.BoxFun.Box.width box' !>! 1 / (200) then
-        trace ("Bisected boxes: " ++ show newBoxes)
-        checkBoxes newBoxes
-      else
-        trace "Stopping bisections (Box too small)" $
-        case (checkTree l box' ac p, checkTree r box' ac p) of
-          (Just True, _) -> Just True
-          (_, Just True) -> Just True
-          (_, _)         -> trace "indeterminate" 
-                            Nothing)
+    if (lRange !>! n || rRange !>! n) then
+      (Just True, Nothing)
+    else 
+      case parallelOr lOptAboveN rOptAboveN of
+        (Just True, _) -> (Just True, Nothing)
+        _ -> 
+          if AERN2.BoxFun.Box.width box' !>! 1 / (10000000) then -- make this threshold quite small (maybe 10^-7)
+              trace ("Bisected boxes: " ++ show newBoxes)
+              checkBoxes newBoxes
+            else
+              trace "Stopping bisections (Box too small)" $
+              case checkTree l box' ac p n of
+                (Just True, _)  -> (Just True, Nothing)
+                _               -> checkTree r box' ac p n -- TODO: Refactor to return nothing instead of False
     where
       lRange = applyTree l box'
       rRange = applyTree r box'
       
-      lOptAboveZero = fromMaybe False (globalMinimumAbove0Tree l box')
-      rOptAboveZero = fromMaybe False (globalMinimumAbove0Tree r box')
+      lOptAboveN = minimumAboveNTree l box' ac p n
+      rOptAboveN = minimumAboveNTree r box' ac p n
  
       box' = setPrecision p box
 
       newBoxes = fullBisect box'
 
-      checkBoxes []           = Just True
-      checkBoxes [b]          = checkTree (Max l r) b ac p
-      checkBoxes (b : boxes)  = (&&) <$> checkBoxes [b]
-                                <*>
-                                checkBoxes boxes
-checkTree (Min l r) box ac p = checkTree l box ac p && checkTree r box ac p
+      checkBoxes []           = (Just True, Nothing)
+      checkBoxes (b : boxes)  = case checkTree (Max l r) b ac p n of
+                                  (Just True, _) -> checkBoxes boxes
+                                  o              -> o
+
+checkTree (Min l r) box ac p n = 
+  parallelAnd (checkTree l box ac p n) (checkTree r box ac p n)
+  -- case checkTree l box ac p n of
+  --   (Just True, _) -> 
+  --     case checkTree r box ac p n of
+  --       (Just True, _) -> (Just True, Nothing)
+  --       o -> o
+  --   o -> o
 
 size :: MinMaxTree -> Integer
 size (Leaf _)  = 1
@@ -91,18 +214,32 @@ size (Max l r) = 1 + size l + size r
 applyTree :: MinMaxTree -> Box -> CN MPBall
 
 -- applyTree (Leaf f)  box = globalMinimumAboveN (BoxFun (dimension (fst f)) (bf_eval (fst f)) box) (bits 100) (prec 1000) (cn 0.0)
-applyTree (Leaf f)  box = apply (f) box
+applyTree (Leaf f)  box = apply f box
 applyTree (Max l r) box = max (applyTree l box) (applyTree r box)
 applyTree (Min l r) box = min (applyTree l box) (applyTree r box)
 
 -- TODO: globalMinimum and globalMaximum with low accuracy/precision to compute more accurate r and lRanges
 
 domainTree :: MinMaxTree -> Box
-domainTree (Leaf f) = domain (f) 
+domainTree (Leaf f) = domain f 
 domainTree (Min l _) = domainTree l 
 domainTree (Max l _) = domainTree l
 
-globalMinimumAbove0Tree :: MinMaxTree -> Box -> Maybe Bool
-globalMinimumAbove0Tree (Leaf f) box  = globalMinimumGreaterThanN (BoxFun (dimension (f)) (bf_eval (f)) box) (bits 10) (prec 10) (cn 0.0)
-globalMinimumAbove0Tree (Max l r) box = (||) <$> globalMinimumAbove0Tree l box <*> globalMinimumAbove0Tree r box
-globalMinimumAbove0Tree (Min l r) box = (&&) <$> globalMinimumAbove0Tree l box <*> globalMinimumAbove0Tree r box
+minimumAboveNTree :: MinMaxTree -> Box -> Accuracy -> Precision -> Rational -> (Maybe Bool, Maybe SearchBox) -- make the cutoff a quarter of a width
+minimumAboveNTree (Leaf f) box ac p n = globalMinimumGreaterThanN (BoxFun (dimension f) (bf_eval f) box) ac p ((width box) / 2) (cn n)
+minimumAboveNTree (Max l r) box ac p n = 
+  parallelOr (minimumAboveNTree l box ac p n) (minimumAboveNTree r box ac p n)
+  -- case minimumAboveNTree l box ac p n of
+  --   (Just True, _) -> (Just True, Nothing)
+  --   _ ->
+  --      case minimumAboveNTree r box ac p n of
+  --        (Just True, _) -> (Just True, Nothing)
+  --        (_, b) -> (Nothing, b)
+minimumAboveNTree (Min l r) box ac p n = 
+  parallelAnd (minimumAboveNTree l box ac p n) (minimumAboveNTree r box ac p n)
+  -- case minimumAboveNTree l box ac p n of
+  --   (Just True, _) ->
+  --     case minimumAboveNTree r box ac p n of
+  --       (Just True, _) -> (Just True, Nothing)
+  --       o -> o
+  --   o -> o
