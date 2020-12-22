a6aa4b14dd52ea7ad8b6bb3915f5d5cc38bbf271
Junaid Rasheed <jrasheed178@gmail.com>
2020-12-22 00:30:59 +0000

    Add more operations to VarMap type

--- aern2-mfun/src/AERN2/BoxFunMinMax/VarMap.hs
+++ aern2-mfun/src/AERN2/BoxFunMinMax/VarMap.hs
@@ -3,8 +3,13 @@ module AERN2.BoxFunMinMax.VarMap where
 import MixedTypesNumPrelude
 import Data.List as L
 import AERN2.BoxFun.Optimisation
-import AERN2.MP.Ball
+import AERN2.MP.Ball (MPBall, endpoints)
 import AERN2.BoxFun.TestFunctions (fromListDomain)
+import AERN2.BoxFun.Box (Box)
+import qualified AERN2.Linear.Vector.Type as V
+
+
+import Debug.Trace
 
 -- | An assosciation list mapping variable names to rational interval domains
 type VarMap = [(String, (Rational, Rational))]
@@ -63,3 +68,108 @@ toSearchBox vMap = SearchBox (fromListDomain (map snd vMap))
 
 centre :: VarMap -> VarMap
 centre = map (\(x,(dL,dR)) -> (x, ((dR+dL)/!2,(dR+dL)/!2)))
+
+-- Precondition, box and varNames have same length
+fromBox :: Box -> [String] -> VarMap
+fromBox box varNames = zip varNames $ V.toList $ V.map (\i -> both (\x -> rational ((~!) x)) (endpoints i)) box
+  where
+    -- From https://hackage.haskell.org/package/extra-1.7.4/docs/src/Data.Tuple.Extra.html#both
+    both :: (a -> b) -> (a, a) -> (b, b)
+    both f (x,y) = (f x, f y)
+
+getCorners :: VarMap -> [VarMap]
+getCorners vm = 
+  nub . map sort $ map (\vm'@(v,_) -> vm' : filter (\(v',_) -> v /= v') rights)  lefts
+                   ++ map (\vm'@(v,_) -> vm' : filter (\(v',_) -> v /= v') lefts)  lefts
+                   ++ map (\vm'@(v,_) -> vm' : filter (\(v',_) -> v /= v') rights) rights 
+                   ++ map (\vm'@(v,_) -> vm' : filter (\(v',_) -> v /= v') lefts)  rights
+  where
+    lefts  = map (\(v,(l,_)) -> (v,(l,l))) vm
+    rights = map (\(v,(_,r)) -> (v,(r,r))) vm
+
+-- Order for two dimension VarMap, left bottom right top
+getEdges :: VarMap -> [VarMap]
+getEdges vm = 
+  nub . map sort $ map (\vm'@(v,_) -> vm' : filter (\(v',_) -> v /= v') vm)  lefts
+                ++ map (\vm'@(v,_) -> vm' : filter (\(v',_) -> v /= v') vm) rights 
+  where
+    lefts  = map (\(v,(l,_)) -> (v,(l,l))) vm
+    rights = map (\(v,(_,r)) -> (v,(r,r))) vm
+
+upperbound :: VarMap -> VarMap
+upperbound = map (\(v,(_,r)) -> (v, (r, r)))
+
+lowerbound :: VarMap -> VarMap
+lowerbound = map (\(v,(l,_)) -> (v, (l, l)))
+
+-- | Get all the possible edges of a given VarMap as a list of VarMaps
+-- Examples:
+-- edges [("x", (0.5, 2.0))]                    = 
+--   [[("x",(1 % 2,1 % 2))],[("x",(2 % 1,2 % 1))]]
+-- edges [("x", (0.5, 2.0)), ("y", (0.8, 1.8))] = 
+--   [[("x",(1 % 2,1 % 2)),("y",(4 % 5,4 % 5))],
+--   [("x",(1 % 2,1 % 2)),("y",(9 % 5,9 % 5))],
+--   [("x",(2 % 1,2 % 1)),("y",(4 % 5,4 % 5))],
+--   [("x",(2 % 1,2 % 1)),("y",(9 % 5,9 % 5))]]
+
+-- [("x", (0.5, 2.0)), ("y" (0.8, 0.8))]
+-- [("x", (0.5, 2.0)), ("y" (1.8, 1.8))]
+-- [("x", (0.5, 0.5)), ("y" (0.8, 1.8))]
+-- [("x", (2.0, 2.0)), ("y" (0.8, 1.8))]
+-- edges :: VarMap -> [VarMap]
+-- edges vs =  (map (\(v, d) -> (filter (\(v', _) -> v /= v') vs)) vs)
+-- where
+--   points = []
+--   points ([(v, (l, r))] : vs = [(v ((l, l), (r, r)))] ++ points vs
+
+-- edges :: VarMap -> [VarMap]
+-- edges vs = 
+--   case L.length vs of
+--     0 -> [[]]
+--     1 -> concatTuple (endpoints (head vs)) []
+--     _ -> 
+--       -- concatMap ((\eps@((v, _), _) -> concatTuple eps (filter (\(v',_) -> v /= v') vs)) . endpoints) vs
+--       -- trace (show (map endpoints vs)) $
+--       -- map (\(l@(v,_), r) -> (filterOutVar v vsEdges)) vsEdges
+--       -- map (\(l@, r)) vsEndpoints
+--       -- joinEdges . sortAllEdges $ map endpoints vs
+--       -- trace (show vsEndpoints) $
+--       [l : leftEndpoints] ++ [r : leftEndpoints] ++ [l : rightEndpoints] ++ [r : rightEndpoints]
+      
+--   where
+--     leftEndpoints = map fst (tail vsEndpoints)
+--     rightEndpoints = map snd (tail vsEndpoints)
+--     vsEndpoints = map endpoints vs
+--     (l, r) = head vsEndpoints
+    
+--     -- fun [] = []
+--     -- fun xs@(l',r') = case L.length xs of
+--       -- 0 -> []
+--       -- 1 -> [l, r]
+      
+
+--     -- vsEdges = (map (\v -> [endpoints v]) vs)
+--     filterOutVar x xs = filter (\(x',_) -> x /= x') xs
+    
+--     -- joinVM vm (l, r) = (l : vm)
+
+--     endpoints (v, (l, r)) = ((v, (l, l)), (v, (r, r)))
+
+--     concatTuple (l, r) xs = [l : xs, r : xs]
+
+--     joinEdges [] = []
+--     joinEdges ((v, d) : es) = 
+--       case filterOutSameVars of
+--         [] -> []
+--         es' ->
+--           (map (\vd -> (v, d) : [vd])) es' ++ joinEdges es
+--       where
+--         filterOutSameVars = (filter (\(v',_) -> v /= v') es)
+
+--     sortAllEdges es = sort . concat $ ls : [rs]
+--       where
+--         ls = map fst es
+--         rs = map snd es
+
+-- [0.5, 0.8, 3.0]
+-- [2.0, 1.8]
