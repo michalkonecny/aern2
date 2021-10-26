2cc538a147c32c5e97ea4b5f43036cbab940544e
Junaid Rasheed <jrasheed178@gmail.com>
2021-10-26 17:23:53 +0100

    Comment out Heron benchmark main file

--- aern2-mfun/bench/Heron.hs
+++ aern2-mfun/bench/Heron.hs
@@ -19,18 +19,19 @@ import System.IO.Unsafe
 main :: IO ()
 main = 
   do
-    args <- getArgs
-    case args of
-      [vc] ->
-        case (vc) of
-          "sine" -> print checkSineVC
-          "heronInit" -> print checkHeronInitExact
-          _ ->
-            print "Not supported"
-      (vc: [i]) ->
-        case (vc) of
-          "heronPreservation" -> print $ checkHeronPreservationExact (read i :: Integer)
-          "heronPreservationYGE" -> print $ checkHeronPreservationExactYGE (read i :: Integer)
-          "heronPreservationYLE" -> print $ checkHeronPreservationExactYLE (read i :: Integer)
-          _ -> print "Not supported"
-      _ -> print "Not supported"
+    print "unimplemented"
+    -- args <- getArgs
+    -- case args of
+    --   [vc] ->
+    --     case (vc) of
+    --       "sine" -> print checkSineVC
+    --       "heronInit" -> print checkHeronInitExact
+    --       _ ->
+    --         print "Not supported"
+    --   (vc: [i]) ->
+    --     case (vc) of
+    --       "heronPreservation" -> print $ checkHeronPreservationExact (read i :: Integer)
+    --       "heronPreservationYGE" -> print $ checkHeronPreservationExactYGE (read i :: Integer)
+    --       "heronPreservationYLE" -> print $ checkHeronPreservationExactYLE (read i :: Integer)
+    --       _ -> print "Not supported"
+    --   _ -> print "Not supported"
