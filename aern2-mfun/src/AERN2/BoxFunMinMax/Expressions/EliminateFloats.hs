5b7503df33ed46e3ee6ee256206ca3d29706a483
Junaid Rasheed <jrasheed178@gmail.com>
2021-12-28 14:20:49 +0000

    Fix issues with eliminateFloats
    + Correctly strengthens formulas with nots, implications, etc.

--- aern2-mfun/src/AERN2/BoxFunMinMax/Expressions/EliminateFloats.hs
+++ aern2-mfun/src/AERN2/BoxFunMinMax/Expressions/EliminateFloats.hs
@@ -5,7 +5,7 @@ import AERN2.BoxFunMinMax.Expressions.Type
 import AERN2.BoxFunMinMax.Expressions.Translators.FPTaylor
 import System.Process
 import System.IO.Unsafe
-import AERN2.BoxFunMinMax.VarMap (VarMap)
+import AERN2.BoxFunMinMax.VarMap (VarMap, prettyShowVarMap)
 import System.Exit 
 import System.IO.Temp
 import GHC.IO.Handle
@@ -27,62 +27,36 @@ removeFloats (RoundToInteger m e) = RoundToInteger m (removeFloats e)
 -- Eqs are turned into <= and >=, and then floats are eliminated
 -- TODO: Test Max,Min
 eliminateFloatsF :: F -> VarMap -> Bool -> FilePath -> IO F
-eliminateFloatsF (FConn op f1 f2) varMap strengthenFormula fptaylorPath = FConn op <$> eliminateFloatsF f1 varMap strengthenFormula fptaylorPath <*> eliminateFloatsF f2 varMap strengthenFormula fptaylorPath
-eliminateFloatsF (FNot f) varMap strengthenFormula fptaylorPath = FNot <$> eliminateFloatsF f varMap strengthenFormula fptaylorPath
-eliminateFloatsF (FComp op e1 e2) varMap True fptaylorPath = 
-  case op of
-    Gt -> FComp op <$> eliminateFloats e1 varMap False fptaylorPath <*> eliminateFloats e2 varMap True fptaylorPath
-    Ge -> FComp op <$> eliminateFloats e1 varMap False fptaylorPath <*> eliminateFloats e2 varMap True fptaylorPath
-    Lt -> FComp op <$> eliminateFloats e1 varMap True fptaylorPath <*> eliminateFloats e2 varMap False fptaylorPath
-    Le -> FComp op <$> eliminateFloats e1 varMap True fptaylorPath <*> eliminateFloats e2 varMap False fptaylorPath
-    Eq -> 
-      eliminateFloatsF
-      (FConn And
-        (FComp Ge e1 e2)
-        (FComp Le e1 e2))
-      varMap
-      True
-      fptaylorPath
-eliminateFloatsF (FComp op e1 e2) varMap False fptaylorPath = 
-  case op of
-    Gt -> FComp op <$> eliminateFloats e1 varMap True fptaylorPath <*> eliminateFloats e2 varMap False fptaylorPath
-    Ge -> FComp op <$> eliminateFloats e1 varMap True fptaylorPath <*> eliminateFloats e2 varMap False fptaylorPath
-    Lt -> FComp op <$> eliminateFloats e1 varMap False fptaylorPath <*> eliminateFloats e2 varMap True fptaylorPath
-    Le -> FComp op <$> eliminateFloats e1 varMap False fptaylorPath <*> eliminateFloats e2 varMap True fptaylorPath
-    Eq -> 
-      eliminateFloatsF
-      (FConn And
-        (FComp Ge e1 e2)
-        (FComp Le e1 e2))
-      varMap
-      False
-      fptaylorPath
-eliminateFloatsF FTrue _ _ _ = return FTrue
-eliminateFloatsF FFalse _ _ _ = return FFalse 
+eliminateFloatsF f varMap strengthenFormula fptaylorPath =
+  aux f strengthenFormula
+  where
+    aux :: F -> Bool -> IO F
+    aux (FConn Impl context goal) strengthenFormula =
+      FConn Impl
+      <$> aux context (not strengthenFormula)
+      <*> aux goal    strengthenFormula
+    aux (FNot f) strengthenFormula = FNot <$> aux f (not strengthenFormula)
+    aux (FComp op e1 e2) strengthenFormula =
+      case op of
+        Gt -> FComp op <$> eliminateFloatsFromExpression e1 strengthenFormula <*> eliminateFloatsFromExpression e2 (not strengthenFormula)
+        Ge -> FComp op <$> eliminateFloatsFromExpression e1 strengthenFormula <*> eliminateFloatsFromExpression e2 (not strengthenFormula)
+        Lt -> FComp op <$> eliminateFloatsFromExpression e1 (not strengthenFormula) <*> eliminateFloatsFromExpression e2 strengthenFormula
+        Le -> FComp op <$> eliminateFloatsFromExpression e1 (not strengthenFormula) <*> eliminateFloatsFromExpression e2 strengthenFormula
+        Eq -> aux (FConn And (FComp Ge e1 e2) (FComp Le e1 e2)) strengthenFormula
+    aux (FConn op e1 e2) strengthenFormula = FConn op <$> aux e1 strengthenFormula <*> aux e2 strengthenFormula
+    aux FTrue  _ = return FTrue
+    aux FFalse _ = return FFalse
 
-eliminateFloats :: E -> VarMap -> Bool -> FilePath -> IO E
-eliminateFloats e@(Float _ _) _ _ _ = error $ "Cannot eliminate Float, precision unknown. Expression: " ++ show e
-eliminateFloats floatE@(Float32 _ e) varMap addError fptaylorPath = do
-  absoluteError <- findAbsoluteErrorUsingFPTaylor floatE varMap fptaylorPath
-  if addError 
-    then return $ EBinOp Add eWithotFloats (Lit absoluteError) 
-    else return $ EBinOp Sub eWithotFloats (Lit absoluteError) 
-  where
-    eWithotFloats = removeFloats e
-eliminateFloats floatE@(Float64 _ e) varMap addError fptaylorPath = do
-  absoluteError <- findAbsoluteErrorUsingFPTaylor floatE varMap fptaylorPath
-  if addError 
-    then return $ EBinOp Add eWithotFloats (Lit absoluteError)
-    else return $ EBinOp Sub eWithotFloats (Lit absoluteError) 
-  where
-    eWithotFloats = removeFloats e
-eliminateFloats (EBinOp op e1 e2) varMap addError fptaylorPath = EBinOp op <$> eliminateFloats e1 varMap addError fptaylorPath <*> eliminateFloats e2 varMap addError fptaylorPath
-eliminateFloats (EUnOp op e) varMap addError fptaylorPath = EUnOp op <$> eliminateFloats e varMap addError fptaylorPath
-eliminateFloats (Lit v) _ _ _ = return $ Lit v
-eliminateFloats (Var v) _ _ _ = return $ Var v
-eliminateFloats Pi      _ _ _ = return Pi
-eliminateFloats (PowI e i) _ _ _ = return $ PowI e i
-eliminateFloats (RoundToInteger m e) varMap addError fptaylorPath = RoundToInteger m <$> eliminateFloats e varMap addError fptaylorPath
+    eliminateFloatsFromExpression e strengthenExpression = 
+      if hasFloatE e 
+        then do
+          absError <- findAbsoluteErrorUsingFPTaylor e varMap fptaylorPath
+          let eWithoutFloats = removeFloats e
+          if strengthenExpression 
+            then return $ EBinOp Sub eWithoutFloats $ Lit absError
+            else return $ EBinOp Add eWithoutFloats $ Lit absError
+        else
+          return e
 
 -- |Made for Float32/64 expressions
 findAbsoluteErrorUsingFPTaylor :: E -> VarMap -> FilePath -> IO Rational
@@ -103,5 +77,3 @@ findAbsoluteErrorUsingFPTaylor e varMap fptaylorPath =
         hPutStr fileHandle fptaylorInput
         _ <- hGetContents fileHandle -- Ensure handler is finished writing before calling FPTaylor
         readProcessWithExitCode fptaylorPath [filePath] []
-
-
