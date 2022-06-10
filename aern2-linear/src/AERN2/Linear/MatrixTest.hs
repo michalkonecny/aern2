d437b2c086ad3c71fa3c82d533079dcfa3cd446c
mikkonecny <mikkonecny@gmail.com>
2022-06-10 16:14:27 +0100

    [linear] remove matrix dependency

--- aern2-linear/src/AERN2/Linear/MatrixTest.hs
+++ aern2-linear/src/AERN2/Linear/MatrixTest.hs
@@ -14,7 +14,7 @@ import qualified Linear.V as LV
 import qualified Linear as L
 import qualified Data.Vector as Vector
 
-import qualified Data.Matrix as Matrix
+-- import qualified Data.Matrix as Matrix
 
 n1 :: Integer
 n1 = 100
@@ -23,7 +23,7 @@ rowsD :: [[Double]]
 rowsD = map (map double) rowsI
   where
   rowsI = [[ item i j  | j <- [1..n1] ] | i <- [1..n1]]
-  item i j 
+  item i j
     | i == j = n1 + i - 1
     | otherwise = 1
 
@@ -46,10 +46,10 @@ m1 = vn1FromList (map vn1FromList rowsD)
 m1Det :: Double
 m1Det = L.luDetFinite m1
 
-{- Using Data.Matrix -}
+-- {- Using Data.Matrix -}
 
-m1DM :: Matrix.Matrix Double
-m1DM = Matrix.fromLists rowsD
+-- m1DM :: Matrix.Matrix Double
+-- m1DM = Matrix.fromLists rowsD
 
-m1DetDM :: Double
-m1DetDM = Matrix.detLU m1DM -- much slower than Linear
+-- m1DetDM :: Double
+-- m1DetDM = Matrix.detLU m1DM -- much slower than Linear
