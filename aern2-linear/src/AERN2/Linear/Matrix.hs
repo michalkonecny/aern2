7a4b7e65b23e1b7f020fb1cf9f2479c274abab52
mikkonecny <mikkonecny@gmail.com>
2022-06-11 11:09:57 +0100

    [linear] naive Laplace matrix determinant

--- aern2-linear/src/AERN2/Linear/Matrix.hs
+++ aern2-linear/src/AERN2/Linear/Matrix.hs
@@ -17,6 +17,8 @@ import qualified Data.Vector as Vector
 import GHC.TypeLits (KnownNat, Nat)
 import Data.Typeable (Typeable)
 import AERN2.Real (CReal, creal)
+import Data.Foldable (Foldable(toList))
+-- import qualified Data.Map as Map
 
 type MatrixRC rn cn e = LV.V (rn :: Nat) (LV.V (cn :: Nat) e)
 
@@ -39,16 +41,54 @@ instance (Show e, Typeable e, KnownNat rn, KnownNat cn) => ConvertibleExactly [[
         Just v -> return v
         _ -> convError "convertExactly to MatrixRC: row of incorrect length" row
 
+detLaplace :: 
+  (HasIntegers e, CanMulBy e Integer, CanAddSameType e, CanMulSameType e) =>
+  MatrixRC rn cn e -> e
+detLaplace mx = 
+  -- fst $ aux submatrixResults0 mask0 0 1 rowsV
+  doRows alternatingSigns (toList mx)
+  where
+  alternatingSigns :: [Integer]
+  alternatingSigns = 1 : aux
+    where
+    aux = (-1) : alternatingSigns
+  -- submatrixResults0 = Map.empty
+  -- aux submatrixResults mask n s [] = (fromInteger_ s, submatrixResults)
+  doRows _mask [] = fromInteger_ 1
+  doRows mask (row:rest) =
+    sum $ zipWith3 doItem (toList row) mask (submasks mask)
+    where
+    doItem item itemSign submask
+      | itemSign == 0 = fromInteger_ 0
+      | otherwise = 
+        item * itemSign * (doRows submask rest)
+  submasks mask = aux mask
+    where
+    aux [] = []
+    aux (b:bs) 
+      | b == 0 = 
+        (0:bs) : (map (b:) (aux bs))
+      | otherwise = 
+        (0:(map negate bs)) : (map (b:) (aux bs))
+
+  {-
+    recurse from top row downwards, 
+    going over all columns whose elements are not certainly zero, 
+    each sub-matrix identified by: number of dropped top rows + a Boolean vector showing active columns
+    memoizing results for all sub-matrices to be reused when the same sub-matrix is needed again
+  -}
+
 {- mini test -}
 
 n1 :: Integer
-n1 = 100
+n1 = 9
 
 rows1I :: [[Integer]]
 rows1I = [[ item i j  | j <- [1..n1] ] | i <- [1..n1]]
   where
   item i j
     | i == j = n1 + i - 1
+    | j > i + 1 = 0
     | otherwise = 1
 
 rows1D :: [[Double]]
@@ -57,13 +97,16 @@ rows1D = map (map double) rows1I
 rows1R :: [[CReal]]
 rows1R = map (map creal) rows1I
 
-type VN1 = LV.V 100
+type VN1 = LV.V 9
 
 m1D :: VN1 (VN1 Double)
 m1D = matrixRC rows1D
 
-m1Det :: Double
-m1Det = L.luDetFinite m1D
+m1DetLU :: Double
+m1DetLU = L.luDetFinite m1D
+
+m1DetLaplace :: Double
+m1DetLaplace = detLaplace m1D
 
 m1R :: VN1 (VN1 CReal)
 m1R = matrixRC rows1R
