762bc95e3e9ec32484159815e376269301125a28
michalkonecny <mikkonecny@gmail.com>
2024-05-29 22:35:45 +0100

    cont principles: shorten some names, return 0 when sequence not used

--- aern2-real/src/AERN2/Continuity/Principles.hs
+++ aern2-real/src/AERN2/Continuity/Principles.hs
@@ -1,6 +1,6 @@
 module AERN2.Continuity.Principles
   ( maxSeqIndexUsed,
-    maxIntParamUsed
+    maxIntParamUsed,
   )
 where
 
@@ -9,47 +9,51 @@ import AERN2.Real (pi)
 import AERN2.Real.CKleenean (CKleenean)
 import AERN2.Real.Comparisons ()
 import AERN2.Real.Type (CSequence (..), creal)
-import GHC.Conc (atomically, newTVar, readTVar, writeTVar, TVar)
+import GHC.Conc (TVar, atomically, newTVar, readTVar, writeTVar)
 import GHC.IO (unsafePerformIO)
 import MixedTypesNumPrelude
 
 -- |
 --    Apply a predicate to a `CSequence`, assuming it returns True
 --    and return the largest index in the sequence that was accessed during the computation.
+--    Index 0 means that the sequence was not accessed at all, 
+--    1 means the first element was accessed, etc.
 maxSeqIndexUsed :: ((CSequence t) -> CKleenean) -> (CSequence t) -> Integer
-maxSeqIndexUsed = maxEffortUsedForPredTrue addCSequenceAccessMonitor
+maxSeqIndexUsed = maxEffortUsed addCSequenceAccessMonitor
 
 -- |
 --    Apply a predicate to a convergent sequence, assuming it returns True
 --    and return the largest index in the sequence that was accessed during the computation.
+--    Index 0 means that the sequence was not accessed at all, 
+--    1 means the sequence was called only with index 0, etc.
 maxIntParamUsed :: ((Integer -> t) -> CKleenean) -> (Integer -> t) -> Integer
-maxIntParamUsed = maxEffortUsedForPredTrue addCallMonitor
+maxIntParamUsed = maxEffortUsed addCallMonitor
 
-maxEffortUsedForPredTrue :: (TVar Integer -> seq -> seq) -> (seq -> CKleenean) -> seq -> Integer
-maxEffortUsedForPredTrue addMonitor f x =
+maxEffortUsed :: (TVar Integer -> seq -> seq) -> (seq -> CKleenean) -> seq -> Integer
+maxEffortUsed addMonitor f x =
   unsafePerformIO $ do
-    maxSoFarTvar <- atomically $ newTVar (-1)
+    maxSoFarTvar <- atomically $ newTVar 0
     let res = f (addMonitor maxSoFarTvar x)
     _ <- waitForTrue (unCSequence res)
     atomically $ readTVar maxSoFarTvar
 
 addCallMonitor :: TVar Integer -> (Integer -> a) -> Integer -> a
 addCallMonitor maxSoFarTvar x i =
-    unsafePerformIO $ do
-    atomically $ do 
-        prevMax <- readTVar maxSoFarTvar
-        writeTVar maxSoFarTvar (prevMax `max` i)
+  unsafePerformIO $ do
+    atomically $ do
+      prevMax <- readTVar maxSoFarTvar
+      writeTVar maxSoFarTvar (prevMax `max` (i + 1)) -- calling x with 0 sets max to 1, etc.
     pure (x i)
 
 addCSequenceAccessMonitor :: TVar Integer -> CSequence t -> CSequence t
-addCSequenceAccessMonitor maxSoFarTvar x = CSequence (monitorSeq 0 (unCSequence x))
-    where
+addCSequenceAccessMonitor maxSoFarTvar x = CSequence (monitorSeq 1 (unCSequence x))
+  where
     monitorSeq _ [] = []
     monitorSeq i (h : t) = monitorH : (monitorSeq (i + 1) t)
-        where
+      where
         monitorH = unsafePerformIO $ do
-            atomically $ writeTVar maxSoFarTvar i
-            pure h
+          atomically $ writeTVar maxSoFarTvar i
+          pure h
 
 waitForTrue :: [CN Kleenean] -> IO (CN Kleenean)
 waitForTrue [] = pure (cn TrueOrFalse)
@@ -60,4 +64,4 @@ waitForTrue (h : t)
       waitForTrue t
 
 test0 :: Integer
-test0 = maxSeqIndexUsed (\x -> x > 0) (pi - pi + (creal (1 / 1000000000))) -- 7
+test0 = maxSeqIndexUsed (\x -> x > 0) (pi - pi + (creal (1 / 1000000000))) -- 8
