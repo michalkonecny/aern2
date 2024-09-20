d4c973b349c0aa59c27fa8fe50199de0ff86f676
Michal Konecny <mikkonecny@gmail.com>
2024-09-20 19:42:54 +0100

    [-affarith] fix string/non-strict order

--- aern2-affarith/src/AERN2/MP/Affine/Order.hs
+++ aern2-affarith/src/AERN2/MP/Affine/Order.hs
@@ -12,35 +12,35 @@ import MixedTypesNumPrelude
 
 instance HasOrderAsymmetric MPAffine MPAffine where
   type OrderCompareType MPAffine MPAffine = Kleenean
-  lessThan aff1 aff2 = mpBall (aff1 - aff2) <= 0
-  leq aff1 aff2 = mpBall (aff1 - aff2) < 0
+  lessThan aff1 aff2 = mpBall (aff1 - aff2) < 0
+  leq aff1 aff2 = mpBall (aff1 - aff2) <= 0
 
 instance HasOrderAsymmetric MPAffine Integer where
   type OrderCompareType MPAffine Integer = Kleenean
-  lessThan aff1 n = mpBall aff1 <= n
-  leq aff1 n = mpBall aff1 < n
+  lessThan aff1 n = mpBall aff1 < n
+  leq aff1 n = mpBall aff1 <= n
 
 instance HasOrderAsymmetric Integer MPAffine where
   type OrderCompareType Integer MPAffine = Kleenean
-  lessThan n aff2 = n <= mpBall aff2
-  leq n aff2 = n < mpBall aff2
+  lessThan n aff2 = n < mpBall aff2
+  leq n aff2 = n <= mpBall aff2
 
 instance HasOrderAsymmetric MPAffine Int where
   type OrderCompareType MPAffine Int = Kleenean
-  lessThan aff1 n = mpBall aff1 <= n
-  leq aff1 n = mpBall aff1 < n
+  lessThan aff1 n = mpBall aff1 < n
+  leq aff1 n = mpBall aff1 <= n
 
 instance HasOrderAsymmetric Int MPAffine where
   type OrderCompareType Int MPAffine = Kleenean
-  lessThan n aff2 = n <= mpBall aff2
-  leq n aff2 = n < mpBall aff2
+  lessThan n aff2 = n < mpBall aff2
+  leq n aff2 = n <= mpBall aff2
 
 instance HasOrderAsymmetric MPAffine Rational where
   type OrderCompareType MPAffine Rational = Kleenean
-  lessThan aff1 q = mpBall aff1 <= q
-  leq aff1 q = mpBall aff1 < q
+  lessThan aff1 q = mpBall aff1 < q
+  leq aff1 q = mpBall aff1 <= q
 
 instance HasOrderAsymmetric Rational MPAffine where
   type OrderCompareType Rational MPAffine = Kleenean
-  lessThan q aff2 = q <= mpBall aff2
-  leq q aff2 = q < mpBall aff2
+  lessThan q aff2 = q < mpBall aff2
+  leq q aff2 = q <= mpBall aff2
